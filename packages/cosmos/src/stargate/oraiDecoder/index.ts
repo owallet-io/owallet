409caf24f9573a467959b3db0fffe3f962dd6b32
ducphamle2 <ducphamle212@gmail.com>
2022-05-16 18:23:39 +0700

    add orai proto, oraicodec & decodec

---
+++ packages/cosmos/src/stargate/oraiDecoder/index.ts
@@ -0,0 +1,75 @@
+import { cosmos } from "../oraiProto";
+import SignDoc = cosmos.tx.v1beta1.SignDoc;
+import { defaultProtoCodec, ProtoCodec } from "../oraiCodec";
+
+export class ProtoSignDocDecoder {
+  public static decode(bytes: Uint8Array): ProtoSignDocDecoder {
+    return new ProtoSignDocDecoder(cosmos.tx.v1beta1.SignDoc.decode(bytes));
+  }
+
+  protected _txBody?: cosmos.tx.v1beta1.TxBody;
+  protected _authInfo?: cosmos.tx.v1beta1.AuthInfo;
+
+  constructor(
+    public readonly signDoc: SignDoc,
+    protected readonly protoCodec: ProtoCodec = defaultProtoCodec
+  ) { }
+
+  get txBody(): cosmos.tx.v1beta1.TxBody {
+    if (!this._txBody) {
+      this._txBody = cosmos.tx.v1beta1.TxBody.decode(this.signDoc.body_bytes);
+    }
+
+    return this._txBody;
+  }
+
+  get txMsgs(): any[] {
+    const msgs: any[] = [];
+    for (const msg of this.txBody.messages) {
+      msgs.push(this.protoCodec.unpackAny(msg));
+    }
+
+    return msgs;
+  }
+
+  get authInfo(): cosmos.tx.v1beta1.AuthInfo {
+    if (!this._authInfo) {
+      this._authInfo = cosmos.tx.v1beta1.AuthInfo.decode(
+        this.signDoc.auth_info_bytes
+      );
+    }
+
+    return this._authInfo;
+  }
+
+  get chainId(): string {
+    return this.signDoc.chain_id;
+  }
+
+  get accountNumber(): string {
+    return this.signDoc.account_number.toString();
+  }
+
+  toBytes(): Uint8Array {
+    return cosmos.tx.v1beta1.SignDoc.encode(this.signDoc).finish();
+  }
+
+  // toJSON(): any {
+  //   return {
+  //     txBody: {
+  //       ...this.txBody.toJSON(),
+  //       ...{
+  //         messages: this.txMsgs.map((msg) => {
+  //           if (msg && msg.toJSON) {
+  //             return msg.toJSON();
+  //           }
+  //           return msg;
+  //         }),
+  //       },
+  //     },
+  //     authInfo: this.authInfo.toJSON(),
+  //     chainId: this.chainId,
+  //     accountNumber: this.accountNumber,
+  //   };
+  // }
+}
