d7e3ddbb85c7aefeba245f0480557f9ef0ed66bf
Pham Tu <tubackkhoa@gmail.com>
2022-06-10 14:32:34 +0700

    watch provider

--- packages/mobile/src/screens/web/config.ts
+++ packages/mobile/src/screens/web/config.ts
@@ -1,6 +1,6 @@
-import { ImageSourcePropType } from "react-native";
+import { ImageSourcePropType } from 'react-native';
 
-export const ORAIDEX_DEV_URL = "http://192.168.1.9:3000";
+export const ORAIDEX_DEV_URL = 'http://192.168.68.92:3000';
 // export const ORAIDEX_DEV_URL = "https://staging.oraidex.io";
 
 export type DAppInfo = {
@@ -12,17 +12,16 @@ export type DAppInfo = {
 
 export const DAppInfos: DAppInfo[] = [
   {
-    name: "Oraidex",
-    thumbnail: require("../../assets/image/webpage/bgoraidex.png"),
-    uri: __DEV__ ? ORAIDEX_DEV_URL : "https://oraidex.io",
-    logo: require("../../assets/image/webpage/orai_logo.png"),
+    name: 'Oraidex',
+    thumbnail: require('../../assets/image/webpage/bgoraidex.png'),
+    uri: __DEV__ ? ORAIDEX_DEV_URL : 'https://oraidex.io',
+    logo: require('../../assets/image/webpage/orai_logo.png')
   },
   {
-    name: "Osmosis",
-    thumbnail: require("../../assets/image/webpage/bgomosis.png"),
-    uri: "https://app.osmosis.zone",
-    logo: require("../../assets/image/webpage/osmosis_logo.png"),
-
-  },
+    name: 'Osmosis',
+    thumbnail: require('../../assets/image/webpage/bgomosis.png'),
+    uri: 'https://app.osmosis.zone',
+    logo: require('../../assets/image/webpage/osmosis_logo.png')
+  }
 ];
  
