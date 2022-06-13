21e5cd7dd33625429c6700f056e0c14f2569f6ad
sonlha <lhas1307@gmail.com>
2022-06-13 10:07:18 +0700

    fix shim for iOS build

--- packages/mobile/shim.js
+++ packages/mobile/shim.js
@@ -1,15 +1,15 @@
-import { polyfillWebCrypto } from "react-native-crypto-polyfill";
+import { polyfillWebCrypto } from 'react-native-crypto-polyfill';
 
 polyfillWebCrypto();
 // crypto is now globally defined
 
-if (typeof __dirname === "undefined") global.__dirname = "/";
-if (typeof __filename === "undefined") global.__filename = "";
-if (typeof process === "undefined") {
-  global.process = require("process");
+if (typeof __dirname === 'undefined') global.__dirname = '/';
+if (typeof __filename === 'undefined') global.__filename = '';
+if (typeof process === 'undefined') {
+  global.process = require('process');
 } else {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const bProcess = require("process");
+  const bProcess = require('process');
   for (const p in bProcess) {
     if (!(p in process)) {
       process[p] = bProcess[p];
@@ -19,24 +19,25 @@ if (typeof process === "undefined") {
 
 process.browser = false;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-if (typeof Buffer === "undefined") global.Buffer = require("buffer").Buffer;
+if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer;
 
 if (!global.atob || !global.btoa) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const Buffer = require("buffer").Buffer;
+  const Buffer = require('buffer').Buffer;
   global.atob = (data) => {
-    return Buffer.from(data, "base64").toString();
+    return Buffer.from(data, 'base64').toString();
   };
 
   global.btoa = (data) => {
-    return Buffer.from(data).toString("base64");
+    return Buffer.from(data).toString('base64');
   };
 }
 
-const isDev = typeof __DEV__ === "boolean" && __DEV__;
-process.env["NODE_ENV"] = isDev ? "development" : "production";
-
-import EventEmitter from "eventemitter3";
+const isDev = typeof __DEV__ === 'boolean' && __DEV__;
+env = process.env ?? {};
+env['NODE_ENV'] = isDev ? 'development' : 'production';
+process.env = env;
+import EventEmitter from 'eventemitter3';
 
 const eventListener = new EventEmitter();
 
