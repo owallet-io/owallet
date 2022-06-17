ea2d7676ec64add7e99c57cf6b614bc7762b4539
Pham Tu <tubackkhoa@gmail.com>
2022-06-17 16:00:50 +0700

    revert no autounlock

--- packages/extension/src/pages/lock/index.tsx
+++ packages/extension/src/pages/lock/index.tsx
@@ -17,8 +17,6 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import { useInteractionInfo } from '@owallet/hooks';
 import { useHistory } from 'react-router';
 import delay from 'delay';
-import { KeyRingStatus } from '@owallet/background';
-import { MainPage } from '../main';
 
 interface FormData {
   password: string;
