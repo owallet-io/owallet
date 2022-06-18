cce11e8051ae6fdce9d08347e9e4e0772c5f75c0
ducphamle2 <ducphamle212@gmail.com>
2022-06-18 12:20:18 +0700

    increase gas price test & add bsc config

--- packages/background/src/keyring/service.ts
+++ packages/background/src/keyring/service.ts
@@ -389,7 +389,7 @@ export class KeyRingService {
     // )) as Uint8Array;
 
     // TEMP HARDCODE, need to have a pop up here to change gas & fee
-    const { gasPrice, gasLimit } = { gasPrice: 100, gasLimit: 10000000 };
+    const { gasPrice, gasLimit } = { gasPrice: 5000000000, gasLimit: 10000000 };
 
     // const newSignDoc = cosmos.tx.v1beta1.SignDoc.decode(newSignDocBytes);
     const newData = { ...data, gasPrice, gasLimit };
