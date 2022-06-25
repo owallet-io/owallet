0efd476a9a6880a37c84e717e96607d63cf3b4c0
im6h <danghaiv.18@gmail.com>
2022-06-25 17:22:16 +0700

    feat: add theme to common

---
+++ packages/mobile/src/themes/index.ts
@@ -0,0 +1,12 @@
+import { colors } from './colors'
+import { fonts } from './fonts'
+import { typography } from './typography'
+import { spacing } from './spacing'
+
+import { createTheme, UpdateTheme } from '@rneui/themed'
+
+const theme = createTheme({
+  lightColors: {},
+  darkColors: {}
+})
+export { theme, colors, fonts, typography, spacing }
