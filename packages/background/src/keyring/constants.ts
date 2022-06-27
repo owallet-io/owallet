2a78812302e4041342f280fd7d16276ed656e31f
ducphamle2 <ducphamle212@gmail.com>
2022-06-27 09:29:50 +0700

    add ethereum base fee

--- packages/background/src/keyring/constants.ts
+++ packages/background/src/keyring/constants.ts
@@ -1,2 +1,2 @@
 export const ROUTE = "keyring";
-export const ETHEREUM_BASE_FEE = 1000000000;
+export const ETHEREUM_BASE_FEE = 1000000000; // reference: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1559.md
