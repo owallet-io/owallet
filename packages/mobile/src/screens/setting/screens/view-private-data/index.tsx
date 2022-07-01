ad5d6da90ed92302771aec0a8094e0a75d9d3c75
im6h <danghaiv.18@gmail.com>
2022-07-01 10:26:46 +0700

    fix: update private key

--- packages/mobile/src/screens/setting/screens/view-private-data/index.tsx
+++ packages/mobile/src/screens/setting/screens/view-private-data/index.tsx
@@ -1,14 +1,16 @@
-import React, { FunctionComponent, useEffect } from "react";
-import { View } from "react-native";
-import { CText as Text} from "../../../../components/text";
-import { useStyle } from "../../../../styles";
-import { CheckIcon } from "../../../../components/icon";
-import { Button } from "../../../../components/button";
-import { WordChip } from "../../../../components/mnemonic";
-import Clipboard from "expo-clipboard";
-import { PageWithScrollViewInBottomTabView } from "../../../../components/page";
-import { useSimpleTimer } from "../../../../hooks";
-import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import React, { FunctionComponent, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { CText as Text } from '../../../../components/text';
+import { useStyle } from '../../../../styles';
+import { CheckIcon, CopyFillIcon } from '../../../../components/icon';
+import { Button } from '../../../../components/button';
+import { WordChip } from '../../../../components/mnemonic';
+import Clipboard from 'expo-clipboard';
+import { PageWithScrollViewInBottomTabView } from '../../../../components/page';
+import { useSimpleTimer } from '../../../../hooks';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { colors, spacing, typography } from '../../../../themes';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const getPrivateDataTitle = (
   keyRingType: string,
@@ -16,15 +18,15 @@ export const getPrivateDataTitle = (
 ) => {
   if (capitalize) {
     return `View ${
-      keyRingType === "mnemonic" ? "Mnemonic Seed" : "Private Key"
+      keyRingType === 'mnemonic' ? 'Mnemonic Seed' : 'Private Key'
     }`;
   }
 
-  return `View ${keyRingType === "mnemonic" ? "mnemonic seed" : "private key"}`;
+  return `View ${keyRingType === 'mnemonic' ? 'mnemonic seed' : 'private key'}`;
 };
 
 export const canShowPrivateData = (keyRingType: string): boolean => {
-  return keyRingType === "mnemonic" || keyRingType === "privateKey";
+  return keyRingType === 'mnemonic' || keyRingType === 'privateKey';
 };
 
 export const ViewPrivateDataScreen: FunctionComponent = () => {
@@ -46,7 +48,7 @@ export const ViewPrivateDataScreen: FunctionComponent = () => {
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
-      title: getPrivateDataTitle(route.params.privateDataType, true),
+      title: getPrivateDataTitle(route.params.privateDataType, true)
     });
   }, [navigation, route.params.privateDataType]);
 
@@ -55,53 +57,79 @@ export const ViewPrivateDataScreen: FunctionComponent = () => {
   const privateData = route.params.privateData;
   const privateDataType = route.params.privateDataType;
 
-  const words = privateData.split(" ");
+  const words = privateData.split(' ');
 
   return (
-    <PageWithScrollViewInBottomTabView
-      style={style.flatten(["padding-x-page"])}
-    >
+    <PageWithScrollViewInBottomTabView>
       <View
-        style={style.flatten([
-          "padding-top-24",
-          "padding-bottom-12",
-          "padding-x-24",
-          "background-color-white",
-        ])}
+        style={{
+          backgroundColor: colors['white'],
+          borderRadius: spacing['24'],
+          paddingHorizontal: 20,
+          paddingVertical: spacing['24'],
+          marginTop: spacing['24']
+        }}
       >
-        <View style={style.flatten(["flex-row", "flex-wrap"])}>
-          {privateDataType === "mnemonic" ? (
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: colors['gray-10'],
+            borderRadius: spacing['24'],
+            padding: spacing['20'],
+            marginBottom: spacing['20']
+          }}
+        >
+          {privateDataType === 'mnemonic' ? (
             words.map((word, i) => {
               return <WordChip key={i.toString()} index={i + 1} word={word} />;
             })
           ) : (
-            <Text style={style.flatten(["h6", "margin-bottom-30"])}>
+            <Text
+              style={{
+                ...typography['h6'],
+                marginBottom: spacing['30']
+              }}
+            >
               {words}
             </Text>
           )}
         </View>
-        <View style={style.flatten(["width-full"])}>
-          <Button
-            textStyle={style.flatten([
-              "text-button1",
-              isTimedOut ? "color-success" : "color-primary",
-            ])}
-            {...(isTimedOut && {
-              rightIcon: (
-                <View style={style.flatten(["margin-left-8"])}>
-                  <CheckIcon />
-                </View>
-              ),
-            })}
-            mode="text"
-            text="Copy to Clipboard"
+        <View
+          style={{
+            width: '100%'
+          }}
+        >
+          <RectButton
+            style={{ ...styles.containerBtn }}
             onPress={() => {
-              Clipboard.setString(words.join(" "));
-              setTimer(3000);
+              Clipboard.setString(words.join(" ").trim())
+              setTimer(2000)
             }}
-          />
+          >
+            <CopyFillIcon color={colors['white']} />
+            <Text
+              style={{ ...styles.textBtn, textAlign: 'center' }}
+            >{`Copy to Clipboard`}</Text>
+          </RectButton>
         </View>
       </View>
     </PageWithScrollViewInBottomTabView>
   );
 };
+
+const styles = StyleSheet.create({
+  containerBtn: {
+    backgroundColor: colors['purple-900'],
+    borderRadius: spacing['8'],
+    paddingVertical: spacing['16'],
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textBtn: {
+    ...typography.h6,
+    color: colors['white'],
+    fontWeight: '700',
+    marginLeft: spacing['8']
+  }
+});
