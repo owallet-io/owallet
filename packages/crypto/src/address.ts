dff14e2d18ea9c624786071247a700a8e52e873b
Pham Tu <tubackkhoa@gmail.com>
2023-03-23 17:09:44 +0700

    fix address

--- packages/crypto/src/address.ts
+++ packages/crypto/src/address.ts
@@ -9,9 +9,9 @@ export class Address {
   }
 
   static getBase58Address(address: string): string {
-    const evmAddress = Buffer.from('41' + address.substring(2), 'hex');
+    const evmAddress = Buffer.from('41' + address.slice(2), 'hex');
     const hash = Hash.sha256(Hash.sha256(evmAddress));
-    const checkSum = hash.slice(0, 4);
+    const checkSum = Buffer.from(hash.slice(0, 4));
     return bs58.encode(Buffer.concat([evmAddress, checkSum]));
   }
 }
