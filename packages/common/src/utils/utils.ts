806c320fc9e2cc147fb298154e0d76349cba2180
sonlha <lhas1307@gmail.com>
2023-03-31 11:41:52 +0700

    remove un-used func

--- packages/common/src/utils/utils.ts
+++ packages/common/src/utils/utils.ts
@@ -1,17 +1,5 @@
-import { Base58 } from '@ethersproject/basex';
-import { sha256 } from '@ethersproject/sha2';
 import bech32, { fromWords } from 'bech32';
 import { ETH } from '@hanchon/ethermint-address-converter';
-
-export const getEvmAddress = base58Address =>
-  '0x' + Buffer.from(Base58.decode(base58Address)).slice(1, -4).toString('hex');
-
-export const getBase58Address = address => {
-  const evmAddress = '0x41' + (address ? address.substring(2) : '');
-  const hash = sha256(sha256(evmAddress));
-  const checkSum = hash.substring(2, 10);
-  return Base58.encode(evmAddress + checkSum);
-};
 
 export const getAddressFromBech32 = bech32address => {
   const address = Buffer.from(fromWords(bech32.decode(bech32address).words));
