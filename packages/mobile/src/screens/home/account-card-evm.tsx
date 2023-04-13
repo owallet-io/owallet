2268e8ad14f9ac696e610a23c76903c2659f8adc
sonlha <lhas1307@gmail.com>
2023-04-13 14:35:23 +0700

    using alias

--- packages/mobile/src/screens/home/account-card-evm.tsx
+++ packages/mobile/src/screens/home/account-card-evm.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, ReactElement } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Card, CardBody } from '../../components/card';
 import { View, ViewStyle, Image } from 'react-native';
-import { CText as Text } from '../../components/text';
+import { Text } from '@src/components/text';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useStore } from '../../stores';
 import { AddressCopyable } from '../../components/address-copyable';
