31720168ebd05c10c8f27ba0765d810ab1e0f3e0
sonlha <lhas1307@gmail.com>
2023-04-27 13:39:37 +0700

    get ledger address

--- packages/mobile/src/screens/home/account-card-evm.tsx
+++ packages/mobile/src/screens/home/account-card-evm.tsx
@@ -1,41 +1,21 @@
-import React, {
-  FunctionComponent,
-  ReactElement,
-  useCallback,
-  useEffect,
-  useState
-} from 'react';
+import React, { FunctionComponent } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Card, CardBody } from '../../components/card';
-import { View, ViewStyle, Image } from 'react-native';
-// import { CText as Text } from '../../components/text';
-
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { ViewStyle } from 'react-native';
 import { useStore } from '../../stores';
 import { AddressCopyable } from '../../components/address-copyable';
 import { useSmartNavigation } from '../../navigation.provider';
-import { DownArrowIcon, SettingDashboardIcon } from '../../components/icon';
-import {
-  BuyIcon,
-  DepositIcon,
-  SendDashboardIcon
-} from '../../components/icon/button';
-import { colors, spacing, typography } from '../../themes';
 import { navigate } from '../../router/root';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { NamespaceModal, AddressQRCodeModal } from './components';
+import { AddressQRCodeModal } from './components';
 import Big from 'big.js';
-import LinearGradient from 'react-native-linear-gradient';
-import MyWalletModal from './components/my-wallet-modal/my-wallet-modal';
-import { NetworkErrorViewEVM } from './network-error-view-evm';
 import { Text } from '@src/components/text';
 import { AccountBox } from './account-box';
 import { TRON_ID } from '@owallet/common';
 import { Address } from '@owallet/crypto';
+import { findLedgerAddressWithChainId, isBase58 } from '@src/utils/helper';
 
 export const AccountCardEVM: FunctionComponent<{
   containerStyle?: ViewStyle;
-}> = observer(({ containerStyle }) => {
+}> = observer(({}) => {
   const {
     chainStore,
     accountStore,
@@ -46,7 +26,6 @@ export const AccountCardEVM: FunctionComponent<{
   } = useStore();
 
   const smartNavigation = useSmartNavigation();
-  // const navigation = useNavigation();
 
   const account = accountStore.getAccount(chainStore.current.chainId);
   const queries = queriesStore.get(chainStore.current.chainId);
@@ -54,27 +33,27 @@ export const AccountCardEVM: FunctionComponent<{
     keyStore => keyStore?.selected
   );
 
-  // const queryStakable = queries.queryBalances.getQueryBech32Address(
-  //   account.bech32Address
-  // ).stakable;
-  // const stakable = queryStakable?.balance;
-  let totalPrice;
   let total;
   if (account.evmosHexAddress) {
-    total = queries.evm.queryEvmBalance.getQueryBalance(
-      account.evmosHexAddress
-    )?.balance;
-
-    if (total) {
-      totalPrice = priceStore?.calculatePrice(total, 'USD');
+    if (keyRingStore.keyRingType === 'ledger') {
+      if (keyRingStore.keyRingLedgerAddresses) {
+        const address = findLedgerAddressWithChainId(
+          keyRingStore.keyRingLedgerAddresses,
+          chainStore.current.chainId
+        );
+        total = queries.evm.queryEvmBalance.getQueryBalance(
+          chainStore.current.chainId === TRON_ID && isBase58(address)
+            ? Address.getEvmAddress(address)
+            : address
+        )?.balance;
+      }
+    } else {
+      total = queries.evm.queryEvmBalance.getQueryBalance(
+        account.evmosHexAddress
+      )?.balance;
     }
   }
-  // const data: [number, number] = [
-  //   parseFloat(stakable.toDec().toString()),
-  //   parseFloat(stakedSum.toDec().toString())
-  // ];
 
-  const safeAreaInsets = useSafeAreaInsets();
   const onPressBtnMain = name => {
     if (name === 'Buy') {
       navigate('MainTab', { screen: 'Browser', path: 'https://oraidex.io' });
@@ -116,7 +95,6 @@ export const AccountCardEVM: FunctionComponent<{
             lineHeight: 50
           }}
         >
-          {/* {console.log(total?.amount?.int?.value,"valueeee")} */}
           {chainStore.current.chainId !== TRON_ID && total
             ? `${new Big(parseInt(total?.amount?.int))
                 .div(new Big(10).pow(36))
@@ -133,7 +111,10 @@ export const AccountCardEVM: FunctionComponent<{
         </Text>
       }
       coinType={`${
-        selected?.bip44HDPath?.coinType ?? chainStore?.current?.bip44?.coinType
+        keyRingStore.keyRingType === 'ledger'
+          ? chainStore?.current?.bip44?.coinType
+          : selected?.bip44HDPath?.coinType ??
+            chainStore?.current?.bip44?.coinType
       }`}
       networkType={'evm'}
       name={account.name || '...'}
@@ -142,7 +123,7 @@ export const AccountCardEVM: FunctionComponent<{
         chainStore.current.chainId !== TRON_ID && total
           ? (
               parseFloat(
-                new Big(parseInt(total?.amount?.int?.value))
+                new Big(parseInt(total?.amount?.int))
                   .div(new Big(10).pow(36))
                   .toString()
               ) *
@@ -164,22 +145,21 @@ export const AccountCardEVM: FunctionComponent<{
           : 0
       }`}
       addressComponent={
-        <>
-          {account.evmosHexAddress ? (
-            <AddressCopyable
-              address={
-                chainStore.current.networkType === 'cosmos'
-                  ? account.bech32Address
-                  : chainStore.current.chainId === TRON_ID &&
-                    account.evmosHexAddress
-                  ? Address.getBase58Address(account.evmosHexAddress)
-                  : account.evmosHexAddress
-              }
-              maxCharacters={22}
-              networkType={chainStore.current.networkType}
-            />
-          ) : null}
-        </>
+        <AddressCopyable
+          address={
+            keyRingStore.keyRingLedgerAddresses &&
+            keyRingStore.keyRingType === 'ledger'
+              ? findLedgerAddressWithChainId(
+                  keyRingStore.keyRingLedgerAddresses,
+                  chainStore.current.chainId
+                )
+              : chainStore.current.chainId === TRON_ID &&
+                account.evmosHexAddress
+              ? Address.getBase58Address(account.evmosHexAddress)
+              : account.evmosHexAddress
+          }
+          maxCharacters={22}
+        />
       }
     />
   );
