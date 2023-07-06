386a39f1c542fc676a3f762e168e6c8ee65b882c
Toan Dang <toan.dq@orai.io>
2023-07-06 16:41:49 +0700

    update all package losted

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -19,7 +19,6 @@ module.exports = {
     ],
     ['@babel/plugin-transform-flow-strip-types'],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-syntax-bigint']
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
   ]
 };
