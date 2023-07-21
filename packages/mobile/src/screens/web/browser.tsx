16697e1204bb78a60659503613a12f4828db5924
sonlha <lhas1307@gmail.com>
2023-07-21 16:48:18 +0700

    manually bundle to build iOS

--- packages/mobile/src/screens/web/browser.tsx
+++ packages/mobile/src/screens/web/browser.tsx
@@ -74,7 +74,7 @@ export const BrowserBookmark: FunctionComponent<{}> = ({}) => {
   );
 };
 
-export const Browser: FunctionComponent<BrowserProps> = observer((props) => {
+export const Browser: FunctionComponent<BrowserProps> = observer(props => {
   const style = useStyle();
   const [isSwitchTab, setIsSwitchTab] = useState(false);
   const navigation = useNavigation();
@@ -125,7 +125,7 @@ export const Browser: FunctionComponent<BrowserProps> = observer((props) => {
     }, 1000);
   }, []);
 
-  const onHandleUrl = (uri) => {
+  const onHandleUrl = uri => {
     let currentUri = uri ?? url;
     if (currentUri !== '') {
       if (checkValidDomain(currentUri?.toLowerCase())) {
@@ -214,9 +214,9 @@ export const Browser: FunctionComponent<BrowserProps> = observer((props) => {
           returnKeyType={'next'}
           placeholder={'Search website'}
           placeholderTextColor={'#AEAEB2'}
-          onSubmitEditing={(e) => onHandleUrl(e.nativeEvent.text)}
+          onSubmitEditing={e => onHandleUrl(e.nativeEvent.text)}
           value={url}
-          onChangeText={(txt) => setUrl(txt.toLowerCase())}
+          onChangeText={txt => setUrl(txt.toLowerCase())}
           inputLeft={
             <TouchableOpacity style={{ paddingRight: 16 }}>
               <SearchLightIcon />
@@ -242,9 +242,9 @@ export const Browser: FunctionComponent<BrowserProps> = observer((props) => {
           }}
         >
           <TouchableOpacity
-            key={`https://orderbook.orai.io/`}
+            key={`https://orderbook.oraidex.io/`}
             style={style.flatten(['height-44', 'margin-bottom-15', 'flex-row'])}
-            onPress={() => onHandleUrl(`https://orderbook.orai.io/`)}
+            onPress={() => onHandleUrl(`https://orderbook.oraidex.io/`)}
           >
             <View style={style.flatten(['padding-top-5'])}>
               <Image
@@ -267,7 +267,7 @@ export const Browser: FunctionComponent<BrowserProps> = observer((props) => {
                 {`Orderbook`}
               </Text>
               <Text style={{ color: colors['sub-text'], fontSize: 14 }}>
-                {`https://orderbook.orai.io/`}
+                {`https://orderbook.oraidex.io/`}
               </Text>
             </View>
           </TouchableOpacity>
