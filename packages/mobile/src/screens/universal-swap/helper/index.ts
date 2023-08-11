82e4e6af7ed0005f3dffba0800dd4ebc8f3a5f60
sonlha <lhas1307@gmail.com>
2023-08-11 17:01:50 +0700

    tron types and erc20 abi

--- packages/mobile/src/screens/universal-swap/helper/index.ts
+++ packages/mobile/src/screens/universal-swap/helper/index.ts
@@ -1,7 +1,7 @@
 import Long from 'long';
 import { network } from '../config/networks';
 import { Address } from '@owallet/crypto';
-
+import { MyBigInt } from '@owallet/common';
 export const calculateTimeoutTimestamp = (timeout: number): string => {
   return Long.fromNumber(Math.floor(Date.now() / 1000) + timeout)
     .multiply(1000000000)
@@ -22,5 +22,27 @@ export const getNetworkGasPrice = async (): Promise<number> => {
   return 0;
 };
 
+export const truncDecimals = 6;
+export const atomic = 10 ** truncDecimals;
+
 export const tronToEthAddress = (base58: string) =>
   Address.getEvmAddress(base58);
+
+export const ethToTronAddress = (address: string) => {
+  return Address.getBase58Address(address);
+};
+
+export const validateNumber = (amount: number | string): number => {
+  if (typeof amount === 'string') return validateNumber(Number(amount));
+  if (Number.isNaN(amount) || !Number.isFinite(amount)) return 0;
+  return amount;
+};
+
+// decimals always >= 6
+export const toAmount = (amount: number | string, decimals = 6): any => {
+  const validatedAmount = validateNumber(amount);
+  // return (
+  //   new MyBigInt(Math.trunc(validatedAmount * atomic)) *
+  //   new MyBigInt(10 ** (decimals - truncDecimals))
+  // );
+};
