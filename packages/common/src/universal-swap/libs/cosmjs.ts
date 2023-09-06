10813a4d4e0d31616d903a586e777af7130905fc
sonlha <lhas1307@gmail.com>
2023-09-06 10:24:17 +0700

    packages/common - comjs export

---
+++ packages/common/src/universal-swap/libs/cosmjs.ts
@@ -0,0 +1,49 @@
+import { MsgExecuteContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx';
+import { toUtf8 } from '@cosmjs/encoding';
+import { Coin } from '@cosmjs/stargate';
+import { EncodeObject } from '@cosmjs/proto-signing';
+
+export interface HandleOptions {
+  readonly memo?: string;
+  readonly funds?: readonly Coin[];
+}
+
+export interface ExecuteMultipleMsg {
+  contractAddress: string;
+  handleMsg: string;
+  handleOptions?: HandleOptions;
+}
+
+interface Msg {
+  contractAddress: string;
+  handleMsg: any;
+  transferAmount?: readonly Coin[];
+}
+
+const parseExecuteContractMultiple = (msgs: ExecuteMultipleMsg[]) => {
+  console.log('messages in parse execute contract: ', msgs);
+  return msgs.map(({ handleMsg, handleOptions, contractAddress }) => {
+    return {
+      handleMsg: JSON.parse(handleMsg),
+      transferAmount: handleOptions?.funds,
+      contractAddress
+    };
+  });
+};
+
+const getExecuteContractMsgs = (
+  senderAddress: string,
+  msgs: Msg[]
+): EncodeObject[] => {
+  return msgs.map(({ handleMsg, transferAmount, contractAddress }) => {
+    return {
+      typeUrl: '/cosmwasm.wasm.v1.MsgExecuteContract',
+      value: MsgExecuteContract.fromPartial({
+        sender: senderAddress,
+        contract: contractAddress,
+        msg: toUtf8(JSON.stringify(handleMsg)),
+        funds: [...(transferAmount || [])]
+      })
+    };
+  });
+};
