184a21335c001f208a6781d78a606a791a33b1ab
sonlha <lhas1307@gmail.com>
2023-09-06 15:48:10 +0700

    libs/cosmjs

--- packages/common/src/universal-swap/libs/cosmjs.ts
+++ packages/common/src/universal-swap/libs/cosmjs.ts
@@ -1,8 +1,13 @@
-import { MsgExecuteContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx';
+import * as cosmwasm from '@cosmjs/cosmwasm-stargate';
 import { toUtf8 } from '@cosmjs/encoding';
-import { Coin } from '@cosmjs/stargate';
 import { EncodeObject } from '@cosmjs/proto-signing';
+import { Coin, GasPrice, isDeliverTxFailure, logs } from '@cosmjs/stargate';
+import { MsgExecuteContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx';
 
+/**
+ * The options of an .instantiate() call.
+ * All properties are optional.
+ */
 export interface HandleOptions {
   readonly memo?: string;
   readonly funds?: readonly Coin[];
@@ -48,4 +53,71 @@ const getExecuteContractMsgs = (
   });
 };
 
-export { getExecuteContractMsgs, parseExecuteContractMultiple };
+const getAminoExecuteContractMsgs = (senderAddress: string, msgs: Msg[]) => {
+  return msgs.map(({ handleMsg, transferAmount, contractAddress }) => {
+    return {
+      type: 'wasm/MsgExecuteContract',
+      value: {
+        sender: senderAddress,
+        contract: contractAddress,
+        msg: handleMsg,
+        sent_funds: [...(transferAmount || [])]
+      }
+    };
+  });
+};
+
+const executeMultipleDirectClient = async (
+  senderAddress: string,
+  msgs: Msg[],
+  memo = '',
+  client: cosmwasm.SigningCosmWasmClient
+) => {
+  const executeContractMsgs = getExecuteContractMsgs(senderAddress, msgs);
+  console.log({ senderAddress, executeContractMsgs });
+  const result = await client.signAndBroadcast(
+    senderAddress,
+    executeContractMsgs,
+    'auto',
+    memo
+  );
+  if (isDeliverTxFailure(result)) {
+    throw new Error(
+      `Error when broadcasting tx ${result.transactionHash} at height ${result.height}. Raw log: ${result.rawLog}`
+    );
+  }
+  return {
+    logs: logs.parseRawLog(result.rawLog),
+    transactionHash: result.transactionHash
+  };
+};
+
+const executeMultipleAminoClient = async (
+  msgs: Msg[],
+  memo = '',
+  client: cosmwasm.SigningCosmWasmClient,
+  walletAddr: string
+) => {
+  const executeMsgs = getExecuteContractMsgs(walletAddr, msgs);
+
+  const result = await client.signAndBroadcast(
+    walletAddr,
+    executeMsgs,
+    'auto',
+    memo
+  );
+  if (isDeliverTxFailure(result)) {
+    throw new Error(
+      `Error when broadcasting tx ${result.transactionHash} at height ${result.height}. Code: ${result.code}; Raw log: ${result.rawLog}`
+    );
+  }
+  return {
+    logs: result?.rawLog,
+    transactionHash: result.transactionHash
+  };
+};
+export {
+  getExecuteContractMsgs,
+  parseExecuteContractMultiple,
+  getAminoExecuteContractMsgs
+};
