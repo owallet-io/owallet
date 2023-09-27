49eb66c2a436e22876ed075a4ee4a5abaf281e12
Toan Dang <toan.dq@orai.io>
2023-09-27 15:58:55 +0700

    add address for tron extension

--- packages/extension/src/pages/main/account.tsx
+++ packages/extension/src/pages/main/account.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useCallback } from 'react';
 
 import { Address } from '../../components/address';
-
+import { Address as Add } from '@owallet/crypto';
 import styleAccount from './account.module.scss';
 
 import { observer } from 'mobx-react-lite';
@@ -9,29 +9,22 @@ import { useStore } from '../../stores';
 import { useNotification } from '../../components/notification';
 import { useIntl } from 'react-intl';
 import { WalletStatus } from '@owallet/stores';
-import { getBase58Address, TRON_ID } from '@owallet/common';
+import { TRON_ID } from '@owallet/common';
 
 export const AccountView: FunctionComponent = observer(() => {
   const { accountStore, chainStore, keyRingStore } = useStore();
   const accountInfo = accountStore.getAccount(chainStore.current.chainId);
-  const selected = keyRingStore?.multiKeyStoreInfo?.find(
-    (keyStore) => keyStore?.selected
-  );
+  const selected = keyRingStore?.multiKeyStoreInfo?.find((keyStore) => keyStore?.selected);
   const intl = useIntl();
   const checkTronNetwork = chainStore.current.chainId === TRON_ID;
   const ledgerAddress =
-    keyRingStore.keyRingType == 'ledger'
-      ? checkTronNetwork
-        ? keyRingStore?.keyRingLedgerAddresses?.trx
-        : keyRingStore?.keyRingLedgerAddresses?.eth
-      : '';
+    keyRingStore.keyRingType == 'ledger' ? (checkTronNetwork ? keyRingStore?.keyRingLedgerAddresses?.trx : keyRingStore?.keyRingLedgerAddresses?.eth) : '';
 
-  const evmAddress =
-    (accountInfo.hasEvmosHexAddress ||
-      chainStore.current.networkType === 'evm') &&
-      checkTronNetwork
-      ? getBase58Address(accountInfo.evmosHexAddress ?? '')
-      : accountInfo.evmosHexAddress;
+  const evmAddress = (accountInfo.hasEvmosHexAddress || chainStore.current.networkType === 'evm') && accountInfo.evmosHexAddress;
+  const tronAddress =
+    (accountInfo.hasEvmosHexAddress || chainStore.current.networkType === 'evm') && checkTronNetwork
+      ? Add.getBase58Address(accountInfo.evmosHexAddress ?? '')
+      : null;
   const notification = useNotification();
 
   const copyAddress = useCallback(
@@ -62,9 +55,9 @@ export const AccountView: FunctionComponent = observer(() => {
         <div className={styleAccount.name}>
           {accountInfo.walletStatus === WalletStatus.Loaded
             ? accountInfo.name ||
-            intl.formatMessage({
-              id: 'setting.keyring.unnamed-account'
-            })
+              intl.formatMessage({
+                id: 'setting.keyring.unnamed-account'
+              })
             : 'Loading...'}
         </div>
         <div style={{ flex: 1, textAlign: 'right' }}>
@@ -88,80 +81,75 @@ export const AccountView: FunctionComponent = observer(() => {
       {chainStore.current.networkType === 'cosmos' && (
         <div className={styleAccount.containerAccount}>
           <div style={{ flex: 1 }} />
-          <div
-            className={styleAccount.address}
-            onClick={() => copyAddress(accountInfo.bech32Address)}
-          >
+          <div className={styleAccount.address} onClick={() => copyAddress(accountInfo.bech32Address)}>
             <span className={styleAccount.addressText}>
               <Address maxCharacters={22} lineBreakBeforePrefix={false}>
-                {accountInfo.walletStatus === WalletStatus.Loaded &&
-                  accountInfo.bech32Address
-                  ? accountInfo.bech32Address
-                  : '...'}
+                {accountInfo.walletStatus === WalletStatus.Loaded && accountInfo.bech32Address ? accountInfo.bech32Address : '...'}
               </Address>
             </span>
             <div style={{ width: 6 }} />
-            <img
-              src={require('../../public/assets/img/filled.svg')}
-              alt="filled"
-              width={16}
-              height={16}
-            />
+            <img src={require('../../public/assets/img/filled.svg')} alt="filled" width={16} height={16} />
           </div>
           <div style={{ flex: 1 }} />
         </div>
       )}
-      {(accountInfo.hasEvmosHexAddress ||
-        chainStore.current.networkType === 'evm') && (
+      {(accountInfo.hasEvmosHexAddress || chainStore.current.networkType === 'evm') && (
+        <div
+          className={styleAccount.containerAccount}
+          style={{
+            marginTop: '2px',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <div style={{ flex: 1 }} />
           <div
-            className={styleAccount.containerAccount}
-            style={{
-              marginTop: '2px',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
+            className={styleAccount.address}
+            style={{ marginBottom: '6px' }}
+            onClick={() => copyAddress(keyRingStore.keyRingType !== 'ledger' ? evmAddress : ledgerAddress)}
           >
-            <div style={{ flex: 1 }} />
-            <div
-              className={styleAccount.address}
-              style={{ marginBottom: '6px' }}
-              onClick={() =>
-                copyAddress(
-                  keyRingStore.keyRingType !== 'ledger'
-                    ? evmAddress
-                    : ledgerAddress
-                )
-              }
-            >
+            {checkTronNetwork && (
+              <span
+                style={{
+                  fontWeight: 'bold'
+                }}
+              >
+                Evmos:
+              </span>
+            )}
+            <span className={styleAccount.addressText}>
+              {keyRingStore.keyRingType !== 'ledger' ? (
+                <Address isRaw={true} tooltipAddress={evmAddress}>
+                  {accountInfo.walletStatus === WalletStatus.Loaded && accountInfo.evmosHexAddress && Add.shortAddress(evmAddress)}
+                </Address>
+              ) : (
+                <Address isRaw={true} tooltipAddress={ledgerAddress}>
+                  {Add.shortAddress(ledgerAddress)}
+                </Address>
+              )}
+            </span>
+            <div style={{ width: 6 }} />
+            <img src={require('../../public/assets/img/filled.svg')} alt="filled" width={16} height={16} />
+          </div>
+          {checkTronNetwork && tronAddress && (
+            <div className={styleAccount.address} style={{ marginBottom: '6px' }} onClick={() => copyAddress(tronAddress)}>
+              <span
+                style={{
+                  fontWeight: 'bold'
+                }}
+              >
+                Base58:
+              </span>
               <span className={styleAccount.addressText}>
-                {keyRingStore.keyRingType !== 'ledger' ? (
-                  <Address isRaw={true} tooltipAddress={evmAddress}>
-                    {accountInfo.walletStatus === WalletStatus.Loaded &&
-                      accountInfo.evmosHexAddress
-                      ? accountInfo.evmosHexAddress.length === 42
-                        ? `${evmAddress.slice(0, 10)}...${evmAddress.slice(-8)}`
-                        : accountInfo.evmosHexAddress
-                      : '...'}
-                  </Address>
-                ) : (
-                  <Address isRaw={true} tooltipAddress={ledgerAddress}>
-                    {ledgerAddress
-                      ? `${ledgerAddress?.slice(0, 10)}...${ledgerAddress?.slice(
-                        -8
-                      )}`
-                      : '...'}
-                  </Address>
-                )}
+                <Address isRaw={true} tooltipAddress={tronAddress}>
+                  {Add.shortAddress(tronAddress)}
+                </Address>
               </span>
               <div style={{ width: 6 }} />
-              <img
-                src={require('../../public/assets/img/filled.svg')}
-                alt="filled"
-                width={16}
-                height={16}
-              />
+              <img src={require('../../public/assets/img/filled.svg')} alt="filled" width={16} height={16} />
             </div>
-            {/* <div
+          )}
+          {/* <div
             className={styleAccount.address}
             onClick={() => copyAddress(accountInfo.evmosHexAddress)}
           >
@@ -177,22 +165,22 @@ export const AccountView: FunctionComponent = observer(() => {
               </Address>
             </span>
           </div> */}
-            <div style={{ flex: 1 }} />
-          </div>
-        )}
+          <div style={{ flex: 1 }} />
+        </div>
+      )}
       <div className={styleAccount.coinType}>
         {' '}
-        {`Coin type: m/44'/${(keyRingStore.keyRingType == 'ledger'
+        {`Coin type: m/44'/${
+          (keyRingStore.keyRingType == 'ledger'
             ? chainStore?.current?.bip44?.coinType
-            : selected?.bip44HDPath?.coinType ??
-            chainStore?.current?.bip44?.coinType) +
+            : selected?.bip44HDPath?.coinType ?? chainStore?.current?.bip44?.coinType) +
           "'/" +
           (selected?.bip44HDPath?.account ?? '0') +
           "'/" +
           (selected?.bip44HDPath?.change ?? '0') +
           '/' +
           (selected?.bip44HDPath?.addressIndex ?? '0')
-          }`}
+        }`}
       </div>
     </div>
   );
