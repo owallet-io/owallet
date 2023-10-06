a02976a05f1fe3ec9cf1076a1eb59dc2c026fba8
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-10-06 14:38:37 +0700

    edit image inj token

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -255,7 +255,7 @@ export const EmbedChainInfos: AppChainInfo[] = [
       coinMinimalDenom: 'inj',
       coinDecimals: 18,
       coinGeckoId: 'injective-protocol',
-      coinImageUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7533.png',
+      coinImageUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7226.png',
       gasPriceStep: {
         low: 5000000000,
         average: 25000000000,
