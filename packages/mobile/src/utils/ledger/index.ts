70ee14e17ad1b651827fe3f05bba9009360a65fd
Toan Dang <toan.dq@orai.io>
2023-10-23 16:43:30 +0700

    clean formatNeworkTypeToLedgerAppName fn

--- packages/mobile/src/utils/ledger/index.ts
+++ packages/mobile/src/utils/ledger/index.ts
@@ -2,37 +2,12 @@ import { LedgerAppType } from '@owallet/background';
 import { TRON_ID } from '@owallet/common';
 import { AsyncKVStore } from '../../common';
 
-export const getLastUsedLedgerDeviceId = async (): Promise<
-  string | undefined
-> => {
+export const getLastUsedLedgerDeviceId = async (): Promise<string | undefined> => {
   const kvStore = new AsyncKVStore('__owallet_ledger_nano_x');
   return await kvStore.get<string>('last_device_id');
 };
 
-export const setLastUsedLedgerDeviceId = async (
-  deviceId: string
-): Promise<void> => {
+export const setLastUsedLedgerDeviceId = async (deviceId: string): Promise<void> => {
   const kvStore = new AsyncKVStore('__owallet_ledger_nano_x');
   await kvStore.set<string>('last_device_id', deviceId);
 };
-
-export function formatNeworkTypeToLedgerAppName(
-  network: string,
-  chainId?: string | number
-): LedgerAppType {
-  
-  switch (network) {
-    case 'cosmos':
-      if((chainId as string).startsWith('injective')){
-        return 'eth';  
-      }
-      return 'cosmos';
-    case 'evm':
-      if (chainId && chainId === TRON_ID) {
-        return 'trx';
-      }
-      return 'eth';
-    default:
-      return 'cosmos';
-  }
-}
