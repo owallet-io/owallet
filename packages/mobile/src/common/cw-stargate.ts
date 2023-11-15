471002d8b7d92e219c8b2cfcc5e764ebd4112a95
sonlha <lhas1307@gmail.com>
2023-11-15 17:40:35 +0700

    CWStargate gasPrice

--- packages/mobile/src/common/cw-stargate.ts
+++ packages/mobile/src/common/cw-stargate.ts
@@ -1,6 +1,8 @@
 import * as cosmwasm from '@cosmjs/cosmwasm-stargate';
+import { GasPrice } from '@cosmjs/stargate';
+import { network } from '@oraichain/oraidex-common';
 export class CWStargate {
-  static async init(account: any, chainId: string, rpc: string) {
+  static async init(account: any, chainId: string, rpc: string, options?: cosmwasm.SigningCosmWasmClientOptions) {
     const owallet = await account.getOWallet();
     if (!owallet) {
       throw new Error("Can't get the owallet API");
@@ -9,7 +11,10 @@ export class CWStargate {
 
     const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(
       rpc,
-      wallet
+      wallet,
+      options ?? {
+        gasPrice: GasPrice.fromString(network.fee.gasPrice + network.denom)
+      }
     );
     return client;
   }
