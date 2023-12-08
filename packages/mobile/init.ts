b087fddb387c4700ebddc02d7f687780c9d39e2b
sonlha <lhas1307@gmail.com>
2023-12-08 14:25:10 +0700

    init window.ethereum and window.tronWeb

--- packages/mobile/init.ts
+++ packages/mobile/init.ts
@@ -7,3 +7,7 @@ import { RNMessageRequesterInternal } from './src/router';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 window.owallet = new OWallet(`${name}-${version}`, new RNMessageRequesterInternal());
+//@ts-ignore
+window.ethereum = new Ethereum(version, 'core', ETH_ID, new RNMessageRequesterInternal());
+//@ts-ignore
+window.tronWeb = new TronWeb(version, 'core', TRON_ID, new RNMessageRequesterInternal());
