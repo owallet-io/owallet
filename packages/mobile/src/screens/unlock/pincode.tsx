821d021391b610fa8324711406a917c3cf22350c
sonlha <lhas1307@gmail.com>
2023-12-14 15:04:35 +0700

    UI for pincode #3

--- packages/mobile/src/screens/unlock/pincode.tsx
+++ packages/mobile/src/screens/unlock/pincode.tsx
@@ -1,26 +1,20 @@
 import React, { FunctionComponent, useCallback, useEffect, useRef, useState } from 'react';
 import { Alert, AppState, AppStateStatus, Image, Platform, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { observer } from 'mobx-react-lite';
+import { metrics } from '@src/themes';
 import { TextInput } from '../../components/input';
 import delay from 'delay';
 import { useStore } from '../../stores';
 import { StackActions, useNavigation } from '@react-navigation/native';
 import { KeyRingStatus } from '@owallet/background';
-import { KeychainStore } from '../../stores/keychain';
 import { AccountStore } from '@owallet/stores';
 import { autorun } from 'mobx';
-import { spacing } from '../../themes';
-import { ProgressBar } from '../../components/progress-bar';
-import CodePush from 'react-native-code-push';
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTheme } from '@src/themes/theme-provider';
 import OWButton from '@src/components/button/OWButton';
-import { PageWithScrollView } from '@src/components/page';
-import { HeaderWelcome, OrText } from '../register/components';
 import SmoothPinCodeInput from 'react-native-smooth-pincode-input';
 import NumericPad from 'react-native-numeric-pad';
 import OWIcon from '@src/components/ow-icon/ow-icon';
+import OWText from '@src/components/text/ow-text';
 
 async function waitAccountLoad(accountStore: AccountStore<any, any, any, any>, chainId: string): Promise<void> {
   if (accountStore.getAccount(chainId).bech32Address) {
@@ -43,6 +37,8 @@ export const PincodeScreen: FunctionComponent = observer(() => {
   const { keyRingStore, keychainStore, accountStore, chainStore, appInitStore, notificationStore } = useStore();
   const navigation = useNavigation();
   const { colors } = useTheme();
+  const styles = styling(colors);
+
   const [downloading, setDownloading] = useState(false);
   const [installing, setInstalling] = useState(false);
   const [loaded, setLoaded] = useState(false);
@@ -158,30 +154,30 @@ export const PincodeScreen: FunctionComponent = observer(() => {
   return !routeToRegisterOnce.current && keyRingStore.status === KeyRingStatus.EMPTY ? (
     <View />
   ) : (
-    <View style={styles.pincode}>
-      <HeaderWelcome
-        style={{
-          marginTop: 0
-        }}
-        title={'Sign in to OWallet'}
-      />
-      <View
-        style={{
-          paddingLeft: 20,
-          paddingRight: 20
-        }}
-      >
+    <View
+      style={{
+        paddingTop: metrics.screenHeight / 7,
+        justifyContent: 'space-between',
+        height: '100%'
+      }}
+    >
+      <View style={styles.aic}>
+        <OWText variant="h2" typo="bold">
+          Enter your passcode
+        </OWText>
         <View
           style={{
-            alignItems: 'center'
+            paddingLeft: 20,
+            paddingRight: 20,
+            paddingTop: 32
           }}
         >
           <SmoothPinCodeInput
             ref={pinRef}
             value={code}
+            codeLength={6}
             cellStyle={{
-              borderBottomWidth: 2,
-              borderColor: colors['text-primary']
+              borderWidth: 0
             }}
             cellStyleFocused={{
               borderColor: colors['sub-text']
@@ -189,83 +185,83 @@ export const PincodeScreen: FunctionComponent = observer(() => {
             placeholder={
               <View
                 style={{
-                  width: 10,
-                  height: 10,
-                  borderRadius: 25,
-                  opacity: 0.3,
-                  backgroundColor: 'blue'
+                  width: 24,
+                  height: 24,
+                  borderRadius: 48,
+                  opacity: 0.1,
+                  backgroundColor: colors['text-black-high']
                 }}
-              ></View>
+              />
             }
             mask={
               <View
                 style={{
-                  width: 10,
-                  height: 10,
-                  borderRadius: 25,
-                  backgroundColor: 'blue'
+                  width: 24,
+                  height: 24,
+                  borderRadius: 48,
+                  opacity: 0.7,
+                  backgroundColor: colors['text-black-high']
                 }}
-              ></View>
+              />
             }
             maskDelay={1000}
             password={true}
             onFulfill={_checkCode}
             onBackspace={code => console.log(code)}
           />
-          <NumericPad
-            ref={numpadRef}
-            numLength={8}
-            buttonSize={60}
-            activeOpacity={0.1}
-            onValueChange={value => setCode(value)}
-            allowDecimal={true}
-            // style={{ backgroundColor: 'black', paddingVertical: 12 }}
-            // buttonAreaStyle={{ backgroundColor: 'gray' }}
-            // buttonItemStyle={{ backgroundColor: 'red' }}
-            buttonTextStyle={{ fontSize: 22, color: colors['sub-text'] }}
-            //@ts-ignore
-            rightBottomButton={<OWIcon name="contact-outline" />}
-            onRightBottomButtonPress={() => {
-              numpadRef.current.clear();
-            }}
-          />
         </View>
-        <View>
-          <OrText />
-          <OWButton
-            disabled={isBiometricLoading || isLoading}
-            label="Use Biometric Authentication"
-            style={styles.useBiometric}
-            onPress={tryBiometric}
-            type="secondary"
-          />
+      </View>
+      <View style={styles.aic}>
+        <View style={styles.rc}>
+          <OWIcon size={14} name="arrow-left" color={colors['purple-900']} />
+          <OWText style={{ paddingLeft: 8 }} variant="h2" weight="600" size={14} color={colors['purple-900']}>
+            Sign in with Face ID
+          </OWText>
         </View>
-        {keychainStore.isBiometryOn && (
-          <View>
-            <OrText />
-            <OWButton
-              disabled={isBiometricLoading || isLoading}
-              label="Use Biometric Authentication"
-              style={styles.useBiometric}
-              onPress={tryBiometric}
-              type="secondary"
-            />
-          </View>
-        )}
+        <NumericPad
+          ref={numpadRef}
+          numLength={8}
+          buttonSize={60}
+          activeOpacity={0.1}
+          onValueChange={value => setCode(value)}
+          allowDecimal={false}
+          // style={{ backgroundColor: 'black', paddingVertical: 12 }}
+          // buttonAreaStyle={{ backgroundColor: 'gray' }}
+          buttonItemStyle={styles.buttonItemStyle}
+          buttonTextStyle={styles.buttonTextStyle}
+          //@ts-ignore
+          rightBottomButton={<OWIcon size={22} name="arrow-left" />}
+          onRightBottomButtonPress={() => {
+            numpadRef.current.clear();
+          }}
+        />
       </View>
     </View>
   );
 });
 
-const styles = StyleSheet.create({
-  useBiometric: {
-    // marginTop: 44
-  },
-  padIcon: {
-    paddingLeft: 10,
-    width: 'auto'
-  },
-  pincode: {
-    marginTop: 44
-  }
-});
+const styling = colors =>
+  StyleSheet.create({
+    useBiometric: {
+      // marginTop: 44
+    },
+    padIcon: {
+      paddingLeft: 10,
+      width: 'auto'
+    },
+    aic: {
+      alignItems: 'center',
+      paddingBottom: 20
+    },
+    rc: {
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    buttonTextStyle: { fontSize: 22, color: colors['text-black-high'], fontFamily: 'SpaceGrotesk-SemiBold' },
+    buttonItemStyle: {
+      backgroundColor: colors['background-light-gray'],
+      width: 110,
+      height: 80,
+      borderRadius: 8
+    }
+  });
