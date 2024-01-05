12e4552e883955d0efd5a02e288278d5e17eb6e3
sonlha <lhas1307@gmail.com>
2024-01-05 17:42:33 +0700

    increase usdc noble fee

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -704,7 +704,12 @@ export const EmbedChainInfos: AppChainInfo[] = [
     stakeCurrency: {
       coinDenom: 'STAKE',
       coinMinimalDenom: 'ustake',
-      coinDecimals: 6
+      coinDecimals: 6,
+      gasPriceStep: {
+        low: 1,
+        average: 1.5,
+        high: 2
+      }
     },
     bip44: {
       coinType: 118
@@ -752,6 +757,7 @@ export const EmbedChainInfos: AppChainInfo[] = [
         }
       }
     ],
+
     features: ['stargate', 'ibc-transfer', 'cosmwasm', 'no-legacy-stdTx'],
     txExplorer: {
       name: 'Mintscan',
