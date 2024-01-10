e0efe7db74d4911a6e5587476407fa03e437efb7
sonlha <lhas1307@gmail.com>
2024-01-10 10:28:47 +0700

    refresh amounts when pull down home screen

--- packages/mobile/src/screens/home/index.tsx
+++ packages/mobile/src/screens/home/index.tsx
@@ -118,7 +118,7 @@ export const HomeScreen: FunctionComponent = observer(props => {
         queries.cosmos.queryUnbondingDelegations.getQueryBech32Address(address).waitFreshResponse()
       ]);
     }
-
+    handleFetchAmounts(accounts);
     setRefreshing(false);
     setRefreshDate(Date.now());
   }, [address, chainStore.current.chainId]);
@@ -126,17 +126,6 @@ export const HomeScreen: FunctionComponent = observer(props => {
   // This section for getting all tokens of all chains
 
   let accounts = {};
-
-  Object.keys(ChainIdEnum).map(key => {
-    let defaultAddress = accountStore.getAccount(ChainIdEnum[key]).bech32Address;
-    if (ChainIdEnum[key] === ChainIdEnum.TRON) {
-      accounts[ChainIdEnum[key]] = getBase58Address(accountStore.getAccount(ChainIdEnum[key]).evmosHexAddress);
-    } else if (defaultAddress.startsWith('evmos')) {
-      accounts[ChainIdEnum[key]] = accountStore.getAccount(ChainIdEnum[key]).evmosHexAddress;
-    } else {
-      accounts[ChainIdEnum[key]] = defaultAddress;
-    }
-  });
 
   const loadTokenAmounts = useLoadTokens(universalSwapStore);
   // handle fetch all tokens of all chains
