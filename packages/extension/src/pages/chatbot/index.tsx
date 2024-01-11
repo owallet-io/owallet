a50c009585bdf576a90e57374c44c722d4bc2495
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-01-11 17:42:54 +0700

    update UI Chatbox page

--- packages/extension/src/pages/chatbot/index.tsx
+++ packages/extension/src/pages/chatbot/index.tsx
@@ -1,11 +1,36 @@
-import { observer } from 'mobx-react-lite';
-import React, { FunctionComponent } from 'react';
-import { HeaderLayout } from '../../layouts';
-import style from './style.module.scss';
+import { observer } from "mobx-react-lite";
+import React, { FunctionComponent } from "react";
+import { HeaderLayout } from "../../layouts";
+import style from "./style.module.scss";
 
 export const ChatbotPage: FunctionComponent = observer(() => {
   return (
     <HeaderLayout showChainName canChangeChainInfo>
+      <main className={style.msgerChat}>
+        <div className={`${style.msg} ${style.leftMsg}`}>
+          <div className={`${style.msgBubble}`}>
+            <div className={`${style.msgInfo}`}>
+              <div className={`${style.msgInfoName}`}>BOT</div>
+              <div className={`${style.msgInfoTime}`}>12:45</div>
+            </div>
+
+            <div className={`${style.msgText}`}>
+              Hi, welcome to OraiBot! Go ahead and send me a message. 😄
+            </div>
+          </div>
+        </div>
+
+        <div className={`${style.msg} ${style.rightMsg}`}>
+          <div className={`${style.msgBubble}`}>
+            <div className={`${style.msgInfo}`}>
+              <div className={`${style.msgInfoName}`}>Alex</div>
+              <div className={`${style.msgInfoTime}`}>12:46</div>
+            </div>
+
+            <div className={`${style.msgText}`}>Deploy ERC-20 Contract</div>
+          </div>
+        </div>
+      </main>
       <div className={style.inputChat}>
         <input type="text" placeholder="Deploy ERC-20 Contract" />
         <button>Send</button>
