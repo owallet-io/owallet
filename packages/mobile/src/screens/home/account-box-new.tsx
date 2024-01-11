9bc4e8f5ecc5da269ee5c494577db1a51b733eb9
sonlha <lhas1307@gmail.com>
2024-01-11 17:52:04 +0700

    multiple percent by 100

--- packages/mobile/src/screens/home/account-box-new.tsx
+++ packages/mobile/src/screens/home/account-box-new.tsx
@@ -104,7 +104,7 @@ export const AccountBoxAll: FunctionComponent<{}> = observer(({}) => {
           </Text>
           <Text style={styles.profit} color={colors[profit < 0 ? 'error-text-body' : 'success-text-body']}>
             {profit < 0 ? '' : '+'}
-            {Number(profit / totalUsd).toFixed(2)}% (${profit ?? 0}) Today
+            {Number((profit / totalUsd) * 100).toFixed(2)}% (${profit ?? 0}) Today
           </Text>
         </View>
         <View style={styles.btnGroup}>
