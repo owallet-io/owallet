04bed1cf16a8733a5c741196e0a7c1b8de8d4350
sonlha <lhas1307@gmail.com>
2024-01-30 16:40:24 +0700

    remove redundant code

--- packages/mobile/src/stores/app_init/init.ts
+++ packages/mobile/src/stores/app_init/init.ts
@@ -1,7 +1,6 @@
 import { observable, action, makeObservable, computed } from 'mobx';
 import { create, persist } from 'mobx-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import moment from 'moment';
 import { CoinGeckoPrices } from '@oraichain/oraidex-common';
 
 export class AppInit {
@@ -14,7 +13,6 @@ export class AppInit {
     date_updated: null | number;
     theme: 'dark' | 'light';
     visibleTabBar?: string;
-    priceFeed: object;
     prices: CoinGeckoPrices<string>;
   };
   @observable
@@ -29,7 +27,6 @@ export class AppInit {
       date_updated: null,
       theme: 'light',
       isAllNetworks: false,
-      priceFeed: {},
       prices: {}
     };
   }
@@ -68,44 +65,6 @@ export class AppInit {
   updatePrices(prices) {
     const tmpPrices = { ...this.initApp.prices, ...prices };
     this.initApp = { ...this.initApp, prices: tmpPrices };
-  }
-
-  @action
-  updatePriceFeed(address, balances) {
-    // TODO: save balances with address
-    let tmpPrice = { ...this.initApp.priceFeed[address] };
-    if (Object.keys(tmpPrice).length === 0) {
-      // Pricefeed is empty, we never call to get balances of this address before
-      tmpPrice = {
-        [Math.floor(Date.now() / 1000)]: balances,
-        [Math.floor(Date.now() / 1000) + 1]: balances
-      };
-      console.log('not yet ?', tmpPrice);
-    } else {
-      const today = moment.unix(Math.floor(Date.now() / 1000));
-      const yesterday = moment.unix(Number(Object.keys(tmpPrice)[0]));
-      console.log('tmpPrice', tmpPrice);
-
-      if (today.isSame(yesterday, 'day')) {
-        // Today is the same day as the day when the last balances were called
-        // Replace the today balances with the new one
-        tmpPrice[Object.keys(tmpPrice)[1] ?? Math.floor(Date.now() / 1000)] = balances;
-        console.log('today again ?', tmpPrice);
-      } else {
-        // Today is not the same day as the day when the last balances were called
-        // Remove the first element of object, which is the outdated data
-        delete tmpPrice[Object.keys(tmpPrice)[0]];
-        // The second element now become first, which is yesterday data
-        // Push new element into object, become today data
-        tmpPrice[Math.floor(Date.now() / 1000)] = balances;
-        console.log('next day ?', tmpPrice);
-      }
-    }
-    // Assign new balances into address
-    const newPriceFeed = { ...this.getInitApp.priceFeed };
-    newPriceFeed[address] = tmpPrice;
-
-    this.initApp = { ...this.initApp, ...{ priceFeed: newPriceFeed } };
   }
 
   @action
