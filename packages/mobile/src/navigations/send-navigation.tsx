b18e55eeeb7c6dabc2b3eba736aadc31820af76c
sonlha <lhas1307@gmail.com>
2024-02-20 14:02:20 +0700

    neutero config

--- packages/mobile/src/navigations/send-navigation.tsx
+++ packages/mobile/src/navigations/send-navigation.tsx
@@ -4,7 +4,6 @@ import useHeaderOptions from '@src/hooks/use-header';
 import { SCREENS, SCREENS_OPTIONS } from '@src/common/constants';
 import TransferTokensScreen from '@src/screens/transfer-tokens/transfer-screen';
 import { UniversalSwapScreen } from '@src/screens/universal-swap';
-// import { UniversalSwapScreen } from '@src/screens/universal-swap';
 const Stack = createStackNavigator();
 export const SendNavigation: FC = () => {
   const handleScreenOptions = ({ route, navigation }) => {
