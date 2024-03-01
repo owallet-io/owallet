877d61097af50a1f71594ea69422676115041048
sonlha <lhas1307@gmail.com>
2024-03-01 15:18:21 +0700

    remove duplicate och

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -132,11 +132,12 @@ export const EmbedChainInfos: AppChainInfo[] = [
           coinGeckoId: "airight",
           coinImageUrl: "https://i.ibb.co/m8mCyMr/airi.png",
         },
+
         {
           type: "cw20",
           coinDenom: "OCH",
           coinMinimalDenom:
-            "cw20:orai1hn8w33cqvysun2aujk5sv33tku4pgcxhhnsxmvnkfvdxagcx0p8qa4l98q:OCH Token",
+            "cw20:orai1hn8w33cqvysun2aujk5sv33tku4pgcxhhnsxmvnkfvdxagcx0p8qa4l98q:OCH",
           contractAddress:
             "orai1hn8w33cqvysun2aujk5sv33tku4pgcxhhnsxmvnkfvdxagcx0p8qa4l98q",
           coinDecimals: 6,
@@ -255,18 +256,6 @@ export const EmbedChainInfos: AppChainInfo[] = [
           coinDecimals: 6,
           coinImageUrl:
             "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
-        },
-        {
-          type: "cw20",
-          coinDenom: "OCH",
-          coinMinimalDenom:
-            "cw20:orai1hn8w33cqvysun2aujk5sv33tku4pgcxhhnsxmvnkfvdxagcx0p8qa4l98q:OCH",
-          contractAddress:
-            "orai1hn8w33cqvysun2aujk5sv33tku4pgcxhhnsxmvnkfvdxagcx0p8qa4l98q",
-          coinDecimals: 6,
-          coinGeckoId: "och",
-          coinImageUrl:
-            "https://assets.coingecko.com/coins/images/34236/standard/orchai_logo_white_copy_4x-8_%281%29.png",
         },
       ];
     },
