55cb82edba0813f88d12bc3c4d951b16f0fd3ada
Toan Dang <toan.dq@orai.io>
2024-03-11 12:05:28 +0700

    polifil for tendermint

--- packages/mobile/shim.js
+++ packages/mobile/shim.js
@@ -1,15 +1,27 @@
-import './polyfill/crypto';
+import "./polyfill/crypto";
+import { chainInfos } from "@oraichain/oraidex-common";
+// polyfill
+Tendermint37Client.detectVersion = () => {};
+Tendermint37Client.prototype.status = function () {
+  const chainInfo = chainInfos.find(
+    (chain) => chain.networkType === "cosmos" && chain.rpc === this.client.url
+  );
+  return {
+    nodeInfo: {
+      network: chainInfo.chainId,
+      version: "",
+    },
+  };
+};
 
-// crypto is now globally defined
+if (typeof __dirname === "undefined") global.__dirname = "/";
+if (typeof __filename === "undefined") global.__filename = "";
 
-if (typeof __dirname === 'undefined') global.__dirname = '/';
-if (typeof __filename === 'undefined') global.__filename = '';
-
-if (typeof process === 'undefined') {
-  global.process = require('process');
+if (typeof process === "undefined") {
+  global.process = require("process");
 } else {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const bProcess = require('process');
+  const bProcess = require("process");
   for (const p in bProcess) {
     if (!(p in process)) {
       process[p] = bProcess[p];
@@ -19,28 +31,28 @@ if (typeof process === 'undefined') {
 
 process.browser = false;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer;
+if (typeof Buffer === "undefined") global.Buffer = require("buffer").Buffer;
 
 if (!global.atob || !global.btoa) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const Buffer = require('buffer').Buffer;
+  const Buffer = require("buffer").Buffer;
   global.atob = (data) => {
-    return Buffer.from(data, 'base64').toString();
+    return Buffer.from(data, "base64").toString();
   };
 
   global.btoa = (data) => {
-    return Buffer.from(data).toString('base64');
+    return Buffer.from(data).toString("base64");
   };
 }
 
-const isDev = typeof __DEV__ === 'boolean' && __DEV__;
+const isDev = typeof __DEV__ === "boolean" && __DEV__;
 env = process.env ?? {};
-import { INJECTED_PROVIDER_URL } from 'react-native-dotenv';
-env['NODE_ENV'] = isDev ? 'development' : 'production';
-env['INJECTED_PROVIDER_URL'] = INJECTED_PROVIDER_URL;
+import { INJECTED_PROVIDER_URL } from "react-native-dotenv";
+env["NODE_ENV"] = isDev ? "development" : "production";
+env["INJECTED_PROVIDER_URL"] = INJECTED_PROVIDER_URL;
 process.env = env;
 
-import EventEmitter from 'eventemitter3';
+import EventEmitter from "eventemitter3";
 
 const eventListener = new EventEmitter();
 
