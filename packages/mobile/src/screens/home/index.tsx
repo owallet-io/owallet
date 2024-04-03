a99e45e024bddd8a9d6e983bbd4ad7e079f684e4
sonlha <lhas1307@gmail.com>
2024-04-03 09:31:26 +0700

    handle with ledger

--- packages/mobile/src/screens/home/index.tsx
+++ packages/mobile/src/screens/home/index.tsx
@@ -49,6 +49,7 @@ export const HomeScreen: FunctionComponent = observer((props) => {
     browserStore,
     appInitStore,
     universalSwapStore,
+    keyRingStore,
   } = useStore();
 
   const scrollViewRef = useRef<ScrollView | null>(null);
