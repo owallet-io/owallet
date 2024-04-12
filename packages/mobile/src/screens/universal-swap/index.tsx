8f3b59b0c94a98283fbfb591255af0bf17491682
sonlha <lhas1307@gmail.com>
2024-04-12 17:34:10 +0700

    minor change

--- packages/mobile/src/screens/universal-swap/index.tsx
+++ packages/mobile/src/screens/universal-swap/index.tsx
@@ -358,7 +358,7 @@ export const UniversalSwapScreen: FunctionComponent = observer(() => {
           tron: getBase58Address(accountTron.evmosHexAddress),
           kwt: accountKawaiiCosmos.bech32Address,
         });
-      }, 2000);
+      }, 1700);
     }
   };
 
@@ -374,7 +374,7 @@ export const UniversalSwapScreen: FunctionComponent = observer(() => {
     return () => {
       if (timeoutId) clearTimeout(timeoutId);
     };
-  }, [accountOrai.bech32Address]);
+  }, [accountOrai.bech32Address, accountKawaiiCosmos.bech32Address]);
 
   useEffect(() => {
     const filteredToTokens = filterNonPoolEvmTokens(
