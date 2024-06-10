a1c1bb928cd33b6c3b7175f94dc6d906c28bfe5f
Toan Dang <toan.dq@orai.io>
2024-06-10 18:03:16 +0700

    add search input

--- packages/extension/src/pages/home/components/tokens-card.tsx
+++ packages/extension/src/pages/home/components/tokens-card.tsx
@@ -1,4 +1,21 @@
 import React from "react";
+import styles from "./style.module.scss";
+
 export const TokensCard = () => {
-  return <h1>Tokens card</h1>;
+  return (
+    <div className={styles.containerTokenCard}>
+      <div className={styles.searchInputContainer}>
+        <img
+          className={styles.iconSearch}
+          src={require("../../../public/assets/images/owallet_search.svg")}
+          alt="Search icon"
+        />
+        <input
+          className={styles.searchInput}
+          name={"search-token"}
+          placeholder="Search by name"
+        />
+      </div>
+    </div>
+  );
 };
