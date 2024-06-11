f51fbd40180e2bff6c14baff8a9b1e7a249731b8
Toan Dang <toan.dq@orai.io>
2024-06-11 07:56:19 +0700

    done ui for token

--- packages/extension/src/pages/home/components/tokens-card.tsx
+++ packages/extension/src/pages/home/components/tokens-card.tsx
@@ -16,6 +16,42 @@ export const TokensCard = () => {
           placeholder="Search by name"
         />
       </div>
+      <div className={styles.listTokens}>
+        {["1", "2"].map((item, index) => (
+          <TokenItem key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const TokenItem = ({ item }) => {
+  return (
+    <div className={styles.tokenItem}>
+      <div className={styles.wrapLeftBlock}>
+        <div className={styles.logoTokenAndChain}>
+          <div className={styles.tokenWrap}>
+            <img
+              className={styles.token}
+              src={require("../../../public/assets/images/default-avatar.png")}
+            />
+            <div className={styles.chainWrap}>
+              <img
+                className={styles.chain}
+                src={require("../../../public/assets/images/default-avatar.png")}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.bodyTokenItem}>
+          <span className={styles.title}>USDT</span>
+          <span className={styles.subTitle}>Oraichain</span>
+        </div>
+      </div>
+      <div className={styles.rightBlock}>
+        <span className={styles.title}>12,655,436.54</span>
+        <span className={styles.subTitle}>$12,654,546.24</span>
+      </div>
     </div>
   );
 };
