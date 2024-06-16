dff6d00a6e2b8f470624814881a05b053fa3c783
Toan Dang <toan.dq@orai.io>
2024-06-16 18:42:35 +0700

    fix length for fetch balance

--- packages/stores/src/query/cosmos/balance/index.ts
+++ packages/stores/src/query/cosmos/balance/index.ts
@@ -92,7 +92,7 @@ export class ObservableQueryCosmosBalances extends ObservableChainQuery<Balances
 
   protected canFetch(): boolean {
     // If bech32 address is empty, it will always fail, so don't need to fetch it.
-    return this.bech32Address.length > 0;
+    return this.bech32Address?.length > 0;
   }
 
   @override
