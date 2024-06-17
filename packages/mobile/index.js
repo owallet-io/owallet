fb470dcc85061bcee01d81380cddae4800bd99e1
Toan Dang <toan.dq@orai.io>
2024-06-17 18:12:34 +0700

    move env to release

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -14,30 +14,24 @@ import CodePush from 'react-native-code-push';
 import { name as appName } from './app.json';
 import firebase from '@react-native-firebase/app';
 import ByteBrew from 'react-native-bytebrew-sdk';
-
-console.log(process.env.BYTE_BREW_ID_ANDROID,"process.env.BYTE_BREW_ID_ANDROID");
-// Initialize the ByteBrew SDK
-if(Platform.OS == "android") {
-  ByteBrew.Initialize(process.env.BYTE_BREW_ID_ANDROID, process.env.BYTE_BREW_SDK_KEY_ANDROID);
-} else if(Platform.OS == "ios") {
-  ByteBrew.Initialize(process.env.BYTE_BREW_ID_IOS, process.env.BYTE_BREW_SDK_KEY_IOS);
-}
+const { App } = require('./src/app');
 const config = {
   apiKey: process.env.API_KEY,
   projectId: 'owallet-829a1',
   messagingSenderId: process.env.SENDER_ID,
   appId: process.env.APP_ID
 };
-
-firebase.initializeApp(config);
-
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('remoteMessage background', remoteMessage);
-});
-
-const { App } = require('./src/app');
-
 if (!__DEV__) {
+  firebase.initializeApp(config);
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('remoteMessage background', remoteMessage);
+  });
+  // Initialize the ByteBrew SDK
+  if (Platform.OS == 'android') {
+    ByteBrew.Initialize(process.env.BYTE_BREW_ID_ANDROID, process.env.BYTE_BREW_SDK_KEY_ANDROID);
+  } else if (Platform.OS == 'ios') {
+    ByteBrew.Initialize(process.env.BYTE_BREW_ID_IOS, process.env.BYTE_BREW_SDK_KEY_IOS);
+  }
   Sentry.init({
     dsn: 'https://ab29c6e64d65418cb3b9f133dc601c23@o1323226.ingest.sentry.io/4504632450023424',
     tracesSampleRate: 0.7,
@@ -52,8 +46,8 @@ if (!__DEV__) {
       'Abort',
       'Network request failed',
       /Bad status on response/,
-      "Operation was cancelled",
-      "App hanging for at least 2000 ms."
+      'Operation was cancelled',
+      'App hanging for at least 2000 ms.'
     ]
   });
 }
