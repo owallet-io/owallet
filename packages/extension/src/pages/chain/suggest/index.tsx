1ef9287b1038c53d48f50d5a64ab5ee077bed79d
sonlha <lhas1307@gmail.com>
2024-06-20 11:31:40 +0700

    sugguest chain modal

--- packages/extension/src/pages/chain/suggest/index.tsx
+++ packages/extension/src/pages/chain/suggest/index.tsx
@@ -1,13 +1,16 @@
 import React, { FunctionComponent, useEffect } from "react";
 import { useHistory } from "react-router";
-import { Button, Alert } from "reactstrap";
-
+import { Alert } from "reactstrap";
 import style from "./style.module.scss";
 import { EmptyLayout } from "../../../layouts/empty-layout";
 import { FormattedMessage } from "react-intl";
 import { useInteractionInfo } from "@owallet/hooks";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../stores";
+import { Button } from "../../../components/common/button";
+import classnames from "classnames";
+import { Text } from "../../../components/common/text";
+import colors from "../../../theme/colors";
 
 export const ChainSuggestedPage: FunctionComponent = observer(() => {
   const { chainSuggestStore, analyticsStore } = useStore();
@@ -32,24 +35,32 @@ export const ChainSuggestedPage: FunctionComponent = observer(() => {
     <EmptyLayout style={{ height: "100%", paddingTop: "80px" }}>
       <div className={style.container}>
         <img
-          src={require("../../../public/assets/orai_wallet_logo.png")}
+          src={require("../../public/assets/images/img_owallet.png")}
           alt="logo"
           style={{ height: "92px", maxWidth: 92, margin: "0 auto" }}
         />
-        <h1 className={style.header}>
+        {/* <h1 className={style.header}> */}
+        <Text size={22} weight={"600"} color={colors["neutral-text-title"]}>
           <FormattedMessage id="chain.suggested.title" />
-        </h1>
+        </Text>
+        {/* </h1> */}
         <p className={style.paragraph}>
-          <FormattedMessage
-            id="chain.suggested.paragraph"
-            values={{
-              host: chainSuggestStore.waitingSuggestedChainInfo?.data.origin,
-              chainId:
-                chainSuggestStore.waitingSuggestedChainInfo?.data.chainId,
-              // eslint-disable-next-line react/display-name
-              b: (...chunks: any) => <b>{chunks}</b>,
-            }}
-          />
+          <Text
+            size={16}
+            weight={"600"}
+            color={colors["primary-surface-default"]}
+          >
+            <FormattedMessage
+              id="chain.suggested.paragraph"
+              values={{
+                host: chainSuggestStore.waitingSuggestedChainInfo?.data.origin,
+                chainId:
+                  chainSuggestStore.waitingSuggestedChainInfo?.data.chainId,
+                // eslint-disable-next-line react/display-name
+                b: (...chunks: any) => <b>{chunks}</b>,
+              }}
+            />
+          </Text>
         </p>
         <div style={{ flex: 1 }} />
         <Alert className={style.warning} color="warning">
@@ -68,7 +79,60 @@ export const ChainSuggestedPage: FunctionComponent = observer(() => {
             </div>
           </div>
         </Alert>
-        <div className={style.buttons}>
+        <div
+          style={{
+            flexDirection: "row",
+            display: "flex",
+            padding: 16,
+            paddingTop: 0,
+          }}
+        >
+          <Button
+            containerStyle={{ marginRight: 8 }}
+            className={classnames(style.button, style.rejectBtn)}
+            color={"danger"}
+            disabled={!chainSuggestStore.waitingSuggestedChainInfo}
+            data-loading={chainSuggestStore.isLoading}
+            onClick={async (e) => {
+              e.preventDefault();
+
+              await chainSuggestStore.reject();
+
+              if (
+                interactionInfo.interaction &&
+                !interactionInfo.interactionInternal
+              ) {
+                window.close();
+              } else {
+                history.push("/");
+              }
+            }}
+          >
+            <FormattedMessage id="chain.suggested.button.reject" />
+          </Button>
+          <Button
+            className={classnames(style.button, style.approveBtn)}
+            disabled={!chainSuggestStore.waitingSuggestedChainInfo}
+            data-loading={chainSuggestStore.isLoading}
+            onClick={async (e) => {
+              e.preventDefault();
+
+              await chainSuggestStore.approve();
+
+              if (
+                interactionInfo.interaction &&
+                !interactionInfo.interactionInternal
+              ) {
+                window.close();
+              } else {
+                history.push("/");
+              }
+            }}
+          >
+            <FormattedMessage id="chain.suggested.button.approve" />
+          </Button>
+        </div>
+        {/* <div className={style.buttons}>
           <Button
             className={style.button}
             // color="danger"
@@ -118,7 +182,7 @@ export const ChainSuggestedPage: FunctionComponent = observer(() => {
           >
             <FormattedMessage id="chain.suggested.button.approve" />
           </Button>
-        </div>
+        </div> */}
       </div>
     </EmptyLayout>
   );
