0b19e0e9470d004fd3bc2de9c5c2b693b2dec433
sonlha <lhas1307@gmail.com>
2024-06-20 15:20:44 +0700

    fix image link on sugguest chain

--- packages/extension/src/pages/chain/suggest/index.tsx
+++ packages/extension/src/pages/chain/suggest/index.tsx
@@ -35,7 +35,7 @@ export const ChainSuggestedPage: FunctionComponent = observer(() => {
     <EmptyLayout style={{ height: "100%", paddingTop: "80px" }}>
       <div className={style.container}>
         <img
-          src={require("../../public/assets/images/img_owallet.png")}
+          src={require("../../../public/assets/images/img_owallet.png")}
           alt="logo"
           style={{ height: "92px", maxWidth: 92, margin: "0 auto" }}
         />
