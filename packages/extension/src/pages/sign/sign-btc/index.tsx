66babbf2cb44090306128ae096acf53aacd18f3e
sonlha <lhas1307@gmail.com>
2024-06-20 11:05:55 +0700

    sign btc main page

--- packages/extension/src/pages/sign/sign-btc/index.tsx
+++ packages/extension/src/pages/sign/sign-btc/index.tsx
@@ -22,8 +22,19 @@ import {
 } from "@owallet/hooks";
 import { useHistory } from "react-router";
 import { ChainIdHelper } from "@owallet/cosmos";
+import { DataModal } from "../modals/data-modal";
+import useOnClickOutside from "../../../hooks/use-click-outside";
+import { Card } from "../../../components/common/card";
+import colors from "../../../theme/colors";
+import { Text } from "../../../components/common/text";
+import { Address } from "../../../components/address";
+import { useLanguage } from "@owallet/common";
 // eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
 const { BitcoinUnit } = require("bitcoin-units");
+import cn from "classnames/bind";
+import { WalletStatus } from "@owallet/stores";
+
+const cx = cn.bind(style);
 
 enum Tab {
   Details,
@@ -41,11 +52,26 @@ export const SignBtcPage: FunctionComponent = observer(() => {
     queriesStore,
     priceStore,
   } = useStore();
+  const language = useLanguage();
+
   const history = useHistory();
   const interactionInfo = useInteractionInfo(() => {
     signInteractionStore.rejectAll();
   });
   const [dataSign, setDataSign] = useState(null);
+  const [dataSetting, setDataSetting] = useState(false);
+  const settingRef = useRef();
+  const dataRef = useRef();
+
+  useOnClickOutside(dataRef, () => {
+    handleCloseDataModal();
+  });
+
+  const handleCloseDataModal = () => {
+    setDataSetting(false);
+    setTab(Tab.Details);
+  };
+
   //TODO: Hard code for chainID with bitcoin;
   const chainId = "bitcoin";
   const accountInfo = accountStore.getAccount(chainId);
@@ -170,6 +196,279 @@ export const SignBtcPage: FunctionComponent = observer(() => {
     },
   };
   const lastestData = isNoSetFee ? dataSign : dataChanged;
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100vw",
+        overflowX: "auto",
+      }}
+    >
+      {/* <div className={cx("setting", openSetting ? "activeSetting" : "", "modal")} ref={settingRef}>
+        <FeeModal onClose={() => setOpenSetting(false)} feeConfig={feeConfig} gasConfig={gasConfig} />
+      </div> */}
+      <div
+        className={cx("setting", dataSetting ? "activeSetting" : "", "modal")}
+        ref={dataRef}
+      >
+        <DataModal
+          onClose={() => {
+            handleCloseDataModal();
+          }}
+          renderData={() => <BtcDataTab data={lastestData} />}
+        />
+      </div>
+      {
+        /*
+         Show the informations of tx when the sign data is delivered.
+         If sign data not delivered yet, show the spinner alternatively.
+         */
+        isLoaded ? (
+          <div className={style.container}>
+            <div style={{ height: "75%", overflow: "scroll", padding: 16 }}>
+              <div
+                className={classnames(style.tabs)}
+                style={{ display: "flex", paddingBottom: 12 }}
+              >
+                <div>
+                  <Text size={16} weight="700">
+                    {"Approve transaction".toUpperCase()}
+                  </Text>
+                </div>
+                <div
+                  onClick={() => {
+                    setDataSetting(true);
+                    setTab(Tab.Data);
+                  }}
+                  style={{
+                    padding: "6px 12px",
+                    backgroundColor: colors["neutral-surface-action3"],
+                    borderRadius: 999,
+                    alignItems: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  <Text weight="600">Raw Data</Text>
+                  <img
+                    src={require("../../public/assets/icon/tdesign_chevron-right.svg")}
+                  />
+                </div>
+              </div>
+              <div
+                className={classnames(style.tabContainer, {
+                  [style.dataTab]: tab === Tab.Data,
+                })}
+              >
+                {tab === Tab.Data && <BtcDataTab data={lastestData} />}
+                {tab === Tab.Details && (
+                  <BtcDetailsTab
+                    priceStore={priceStore}
+                    feeConfig={feeConfig}
+                    gasConfig={gasConfig}
+                    intl={intl}
+                    dataSign={lastestData}
+                    isNoSetFee={isNoSetFee}
+                  />
+                )}
+              </div>
+              <Card
+                containerStyle={{
+                  borderRadius: 12,
+                  border: "1px solid" + colors["neutral-border-default"],
+                  padding: 8,
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                  onClick={() => {}}
+                >
+                  <Text weight="600">Transaction fee</Text>
+                  <div
+                    style={{
+                      flexDirection: "column",
+                      display: "flex",
+                      alignItems: "flex-end",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        cursor: "pointer",
+                      }}
+                    >
+                      <Text
+                        size={16}
+                        weight="600"
+                        color={colors["primary-text-action"]}
+                      >
+                        {feeConfig.fee.maxDecimals(6).trim(true).toString() ||
+                          0}
+                      </Text>
+                      <img
+                        src={require("../../public/assets/icon/tdesign_chevron-down.svg")}
+                      />
+                    </div>
+                    <Text color={colors["neutral-text-body"]}>
+                      ≈
+                      {priceStore
+                        .calculatePrice(feeConfig.fee, language.fiatCurrency)
+                        ?.toString() || 0}
+                    </Text>
+                  </div>
+                </div>
+              </Card>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                bottom: 0,
+                width: "100%",
+                height: "25%",
+                backgroundColor: colors["neutral-surface-card"],
+                borderTop: "1px solid" + colors["neutral-border-default"],
+              }}
+            >
+              {keyRingStore.keyRingType === "ledger" &&
+              signInteractionStore.isLoading ? (
+                <Button className={style.button} disabled={true} mode="outline">
+                  <FormattedMessage id="sign.button.confirm-ledger" />{" "}
+                  <i className="fa fa-spinner fa-spin fa-fw" />
+                </Button>
+              ) : (
+                <div>
+                  <div
+                    style={{
+                      flexDirection: "row",
+                      display: "flex",
+                      padding: 8,
+                      justifyContent: "space-between",
+                      backgroundColor: colors["neutral-surface-bg"],
+                      margin: 16,
+                      marginBottom: 8,
+                      borderRadius: 12,
+                    }}
+                  >
+                    <div
+                      style={{
+                        flexDirection: "row",
+                        display: "flex",
+                      }}
+                    >
+                      <img
+                        style={{
+                          width: 40,
+                          height: 40,
+                          borderRadius: 40,
+                          marginRight: 8,
+                        }}
+                        src={require("../../public/assets/images/default-avatar.png")}
+                      />
+                      <div style={{ flexDirection: "column", display: "flex" }}>
+                        <Text size={14} weight="600">
+                          {accountInfo.name}
+                        </Text>
+                        <Text color={colors["neutral-text-body"]}>
+                          {" "}
+                          <Address
+                            maxCharacters={18}
+                            lineBreakBeforePrefix={false}
+                          >
+                            {accountInfo.walletStatus === WalletStatus.Loaded &&
+                            signer
+                              ? signer
+                              : "..."}
+                          </Address>
+                        </Text>
+                      </div>
+                    </div>
+                    {/* <Text color={colors["neutral-text-body"]}>123</Text> */}
+                  </div>
+                  <div
+                    style={{
+                      flexDirection: "row",
+                      display: "flex",
+                      padding: 16,
+                      paddingTop: 0,
+                    }}
+                  >
+                    <Button
+                      containerStyle={{ marginRight: 8 }}
+                      className={classnames(style.button, style.rejectBtn)}
+                      // disabled={signDocHelper.signDocWrapper == null}
+                      color={"danger"}
+                      data-loading={signInteractionStore.isLoading}
+                      loading={signInteractionStore.isLoading}
+                      onClick={async (e) => {
+                        e.preventDefault();
+
+                        await signInteractionStore.reject();
+                        if (
+                          interactionInfo.interaction &&
+                          !interactionInfo.interactionInternal
+                        ) {
+                          window.close();
+                        }
+                        history.goBack();
+                      }}
+                    >
+                      {intl.formatMessage({
+                        id: "sign.button.reject",
+                      })}
+                    </Button>
+                    <Button
+                      className={classnames(style.button, style.approveBtn)}
+                      disabled={approveIsDisabled}
+                      data-loading={signInteractionStore.isLoading}
+                      loading={signInteractionStore.isLoading}
+                      onClick={async (e) => {
+                        e.preventDefault();
+
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        //@ts-ignore
+                        await signInteractionStore.approveBitcoinAndWaitEnd({
+                          ...lastestData.data.data,
+                        });
+
+                        if (
+                          interactionInfo.interaction &&
+                          !interactionInfo.interactionInternal
+                        ) {
+                          window.close();
+                        }
+                        history.goBack();
+                      }}
+                    >
+                      {intl.formatMessage({
+                        id: "sign.button.approve",
+                      })}
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <i className="fas fa-spinner fa-spin fa-2x text-gray" />
+          </div>
+        )
+      }
+    </div>
+  );
   return (
     <div
       style={{
