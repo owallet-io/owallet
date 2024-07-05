7f992626aecee26348931175a603884eafac8b79
sonlha <lhas1307@gmail.com>
2024-07-05 10:20:16 +0700

    optiminized #2

--- packages/extension/src/pages/sign/components/render-cosmos-args.tsx
+++ packages/extension/src/pages/sign/components/render-cosmos-args.tsx
@@ -9,44 +9,7 @@ import { Coin, CoinUtils } from "@owallet/unit";
 import { clearDecimals } from "../messages";
 import { useStore } from "../../../stores";
 
-const styles = {
-  container: {
-    marginTop: 14,
-    height: "auto",
-    alignItems: "center",
-  },
-  row: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 14,
-  },
-  column: {
-    flexDirection: "column",
-    display: "flex",
-  },
-  separator: {
-    width: "100%",
-    height: 1,
-    backgroundColor: colors["neutral-border-default"],
-  },
-  image: {
-    width: 14,
-    height: 14,
-    borderRadius: 28,
-    marginRight: 4,
-  },
-  viewMore: {
-    display: "flex",
-    flexDirection: "row",
-    cursor: "pointer",
-    justifyContent: "flex-end",
-    width: "100%",
-    marginTop: 8,
-  },
-};
-
-const TokenRenderer = React.memo(({ token }) => (
+const TokenRenderer: FunctionComponent<{ token: any }> = ({ token }) => (
   <div
     style={{
       display: "flex",
@@ -55,93 +18,127 @@ const TokenRenderer = React.memo(({ token }) => (
       margin: "4px 0",
     }}
   >
-    {(token?.imgUrl || token?.coinImageUrl) && (
-      <img style={styles.image} src={token?.imgUrl ?? token?.coinImageUrl} />
-    )}
+    {token?.imgUrl || token?.coinImageUrl ? (
+      <img
+        style={{ width: 14, height: 14, borderRadius: 28, marginRight: 4 }}
+        src={token?.imgUrl ?? token?.coinImageUrl}
+      />
+    ) : null}
     <Text weight="600">{token?.abbr ?? token?.coinDenom ?? token.denom}</Text>
   </div>
-));
+);
 
-const TokenPathRenderer = React.memo(
-  ({ inToken, outToken, inContract, outContract }) => (
-    <div style={styles.container}>
-      <div style={styles.row}>
-        <div style={{ maxWidth: "50%" }}>
-          <div style={styles.column}>
-            <Text color={colors["neutral-text-body"]}>Pay token</Text>
-            {inToken ? (
-              <TokenRenderer token={inToken} />
-            ) : (
-              <Text color={colors["neutral-text-body"]}>-</Text>
-            )}
-            {inContract && inContract !== "-" ? (
-              <Address
-                maxCharacters={8}
-                lineBreakBeforePrefix={false}
-                textDecor={"underline"}
-                textColor={colors["neutral-text-body"]}
-              >
-                {inContract}
-              </Address>
-            ) : (
-              <Text color={colors["neutral-text-body"]}>-</Text>
-            )}
-          </div>
-        </div>
-        <img
-          style={{ paddingRight: 4 }}
-          src={require("assets/icon/tdesign_arrow-right.svg")}
-        />
-        <div style={{ maxWidth: "50%" }}>
-          <div style={styles.column}>
-            <Text color={colors["neutral-text-body"]}>Receive token</Text>
-            {outToken ? (
-              <TokenRenderer token={outToken} />
-            ) : (
-              <Text color={colors["neutral-text-body"]}>-</Text>
-            )}
-            {outContract && outContract !== "-" ? (
-              <Address
-                maxCharacters={8}
-                lineBreakBeforePrefix={false}
-                textDecor={"underline"}
-                textColor={colors["neutral-text-body"]}
-              >
-                {outContract}
-              </Address>
-            ) : (
-              <Text color={colors["neutral-text-body"]}>-</Text>
-            )}
-          </div>
+const PathRenderer: FunctionComponent<{
+  inToken?: any;
+  outToken?: any;
+  inContract?: string;
+  outContract?: string;
+}> = ({ inToken, outToken, inContract, outContract }) => (
+  <div style={{ marginTop: 14, height: "auto", alignItems: "center" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 14,
+      }}
+    >
+      <div style={{ maxWidth: "50%" }}>
+        <div style={{ flexDirection: "column", display: "flex" }}>
+          <Text color={colors["neutral-text-body"]}>Pay token</Text>
+          {inToken ? (
+            <TokenRenderer token={inToken} />
+          ) : (
+            <Text color={colors["neutral-text-body"]}>-</Text>
+          )}
+          {inContract && inContract !== "-" ? (
+            <Address
+              maxCharacters={8}
+              lineBreakBeforePrefix={false}
+              textDecor={"underline"}
+              textColor={colors["neutral-text-body"]}
+            >
+              {inContract}
+            </Address>
+          ) : (
+            <Text color={colors["neutral-text-body"]}>-</Text>
+          )}
         </div>
       </div>
-      <div style={styles.separator} />
+      <img
+        style={{ paddingRight: 4 }}
+        src={require("assets/icon/tdesign_arrow-right.svg")}
+      />
+      <div style={{ maxWidth: "50%" }}>
+        <div style={{ flexDirection: "column", display: "flex" }}>
+          <Text color={colors["neutral-text-body"]}>Receive token</Text>
+          {outToken ? (
+            <TokenRenderer token={outToken} />
+          ) : (
+            <Text color={colors["neutral-text-body"]}>-</Text>
+          )}
+          {outContract && outContract !== "-" ? (
+            <Address
+              maxCharacters={8}
+              lineBreakBeforePrefix={false}
+              textDecor={"underline"}
+              textColor={colors["neutral-text-body"]}
+            >
+              {outContract}
+            </Address>
+          ) : (
+            <Text color={colors["neutral-text-body"]}>-</Text>
+          )}
+        </div>
+      </div>
     </div>
-  )
+    <div
+      style={{
+        width: "100%",
+        height: 1,
+        backgroundColor: colors["neutral-border-default"],
+      }}
+    />
+  </div>
 );
 
-const InfoRenderer = React.memo(
-  ({ condition, label, leftContent }) =>
-    condition && (
-      <div style={styles.container}>
-        <div style={styles.row}>
-          <div>
-            <Text weight="600">{label}</Text>
-          </div>
-          <div
-            style={{
-              alignItems: "flex-end",
-              maxWidth: "65%",
-              wordBreak: "break-all",
-            }}
-          >
-            {leftContent}
-          </div>
+const InfoRenderer: FunctionComponent<{
+  condition: any;
+  label: string;
+  leftContent: JSX.Element;
+}> = ({ condition, label, leftContent }) =>
+  condition && condition !== "" ? (
+    <div style={{ marginTop: 14, height: "auto", alignItems: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginBottom: 14,
+        }}
+      >
+        <div>
+          <Text weight="600">{label}</Text>
         </div>
-        <div style={styles.separator} />
+        <div
+          style={{
+            alignItems: "flex-end",
+            maxWidth: "65%",
+            wordBreak: "break-all",
+          }}
+        >
+          <div>{leftContent}</div>
+        </div>
       </div>
-    )
-);
+      <div
+        style={{
+          width: "100%",
+          height: 1,
+          backgroundColor: colors["neutral-border-default"],
+        }}
+      />
+    </div>
+  ) : null;
 
 export const CosmosRenderArgs: FunctionComponent<{
   msg: any;
@@ -161,17 +158,13 @@ export const CosmosRenderArgs: FunctionComponent<{
 
   let minimum_receive;
   let ask_asset_info;
-  if (
+  const execute_swap_operations =
     txInfo.extraInfo?.execute_swap_operations ||
-    txInfo.decode?.execute_swap_operations
-  ) {
-    const execute_swap_operations =
-      txInfo.extraInfo?.execute_swap_operations ||
-      txInfo.decode?.execute_swap_operations;
+    txInfo.decode?.execute_swap_operations;
+  if (execute_swap_operations) {
     const lastDes = execute_swap_operations.operations.pop();
     const ask_asset = lastDes.orai_swap?.ask_asset_info?.token?.contract_addr;
     minimum_receive = execute_swap_operations.minimum_receive;
-
     if (ask_asset) {
       EmbedChainInfos.forEach((c) => {
         if (c.chainId === chain?.chainId) {
@@ -270,10 +263,10 @@ export const CosmosRenderArgs: FunctionComponent<{
       />
       {tokensIn.length > 0 ? (
         tokensIn.map((token, index) => (
-          <TokenPathRenderer key={index} inToken={token} />
+          <PathRenderer key={index} inToken={token} />
         ))
       ) : (
-        <TokenPathRenderer />
+        <PathRenderer />
       )}
       {!isMore && (
         <>
@@ -332,20 +325,23 @@ export const CosmosRenderArgs: FunctionComponent<{
           />
         </>
       )}
-      <div style={styles.viewMore}>
-        <div
-          style={{ display: "flex", flexDirection: "row", cursor: "pointer" }}
-          onClick={() => setIsMore(!isMore)}
-        >
-          <Text size={14} weight="500">{`View ${
-            isMore ? "more" : "less"
-          }`}</Text>
-          <img
-            src={require(`assets/icon/tdesign_chevron-${
-              isMore ? "down" : "up"
-            }.svg`)}
-          />
-        </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          cursor: "pointer",
+          justifyContent: "flex-end",
+          width: "100%",
+          marginTop: 8,
+        }}
+        onClick={() => setIsMore(!isMore)}
+      >
+        <Text size={14} weight="500">{`View ${isMore ? "more" : "less"}`}</Text>
+        <img
+          src={require(`assets/icon/tdesign_chevron-${
+            isMore ? "down" : "up"
+          }.svg`)}
+        />
       </div>
     </div>
   );
