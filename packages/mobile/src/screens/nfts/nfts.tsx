2113256b04f7532db3150d0f3d5a8d7336c82587
Toan Dang <toan.dq@orai.io>
2024-07-10 11:49:58 +0700

    handle nfts for oraichain chain

--- packages/mobile/src/screens/nfts/nfts.tsx
+++ packages/mobile/src/screens/nfts/nfts.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import { observer } from "mobx-react-lite";
 import {
   FlatList,
@@ -26,120 +26,143 @@ import { useStore } from "@src/stores";
 import images from "@src/assets/images";
 // import { useSoulbound } from "./hooks/useSoulboundNft";
 import OWFlatList from "@src/components/page/ow-flat-list";
+import { PageHeader } from "@src/components/header/header-new";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { ChainInfo } from "@owallet/types";
+import { NftItem } from "./components/nft-item";
+import { API } from "@src/common/api";
+import { OWEmpty } from "@src/components/empty";
+import { urlAiRight } from "@src/common/constants";
+import { processDataOraiNft } from "./hooks/useNfts";
 // import { SkeletonNft } from "../home/tokens-card";
 export const NftsScreen: FunctionComponent = observer((props) => {
-  const { chainStore, accountStore } = useStore();
-  const account = accountStore.getAccount(chainStore.current.chainId);
+  const { accountStore } = useStore();
 
-  const smartNavigation = useSmartNavigation();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const page = useRef(0);
+  const hasMore = useRef(true);
+  const perPage = 10;
   const { colors } = useTheme();
   const styles = styling(colors);
 
+  const route = useRoute<
+    RouteProp<
+      Record<
+        string,
+        {
+          chainInfo: ChainInfo;
+        }
+      >,
+      string
+    >
+  >();
+
+  const chainInfo = route.params?.chainInfo;
+  const account = accountStore.getAccount(chainInfo.chainId);
+
+  const renderItem = ({ item, index }) => {
+    return <NftItem key={`item-${index + 1}-${index}`} item={item} />;
+  };
+  const fetchData = async (address, isLoadMore = false) => {
+    try {
+      if (!isLoadMore) setLoading(true);
+      if (!hasMore.current) throw Error("Failed");
+
+      let res = await API.getNftsOraichain(
+        {
+          offset: !isLoadMore ? 0 : page.current * perPage,
+          size: perPage,
+          address: address,
+        },
+        {
+          baseURL: urlAiRight,
+        }
+      );
+
+      if (res && res.status !== 200) throw Error("Failed");
+      page.current += 1;
+      console.log(res?.data, "res?.data");
+      const totalPage = Math.ceil(res?.data?.total / perPage);
+      if (page.current === totalPage) {
+        hasMore.current = false;
+      }
+      if (res?.data?.items?.length < 1) {
+        hasMore.current = false;
+      }
+      const nfts = (res?.data?.items || []).map((nft, index) =>
+        processDataOraiNft(nft, chainInfo.currencies)
+      );
+      setData((prevData) => {
+        if (isLoadMore) return [...prevData, ...nfts];
+        return nfts;
+      });
+      setAllLoading();
+    } catch (error) {
+      setAllLoading();
+    }
+  };
+  const setAllLoading = () => {
+    setLoadMore(false);
+    setLoading(false);
+    setRefreshing(false);
+  };
+  useEffect(() => {
+    if (!account.bech32Address) return;
+    page.current = 0;
+    hasMore.current = true;
+    fetchData(account.bech32Address, false);
+    return () => {
+      setData([]);
+    };
+  }, [account.bech32Address]);
+  const onEndReached = () => {
+    if (page.current !== 0 && data.length > 0) {
+      setLoadMore(true);
+      fetchData(account.bech32Address, true);
+      return;
+    }
+  };
+  const onRefresh = () => {
+    setRefreshing(true);
+    page.current = 0;
+    hasMore.current = true;
+    fetchData(account.bech32Address, false);
+  };
+
   return (
-    // <PageWithView>
-    //   <OWBox style={[styles.container]}>
-    //     {/*<SearchFilter />*/}
-    //     <OWFlatList
-    //       data={data}
-    //       onEndReached={onEndReached}
-    //       renderItem={renderItem}
-    //       loadMore={loadMore}
-    //       loading={loading}
-    //       onRefresh={onRefresh}
-    //       ListEmptyComponent={<EmptyTx />}
-    //       refreshing={refreshing}
-    //     />
-    //   </OWBox>
-    // </PageWithView>
-    <></>
+    <PageWithView>
+      <PageHeader title="My NFTs" subtitle={chainInfo?.chainName} />
+      <OWBox style={[styles.container]}>
+        <OWFlatList
+          data={data}
+          onEndReached={onEndReached}
+          renderItem={renderItem}
+          columnWrapperStyle={styles.row}
+          numColumns={2}
+          loadMore={loadMore}
+          loading={loading}
+          onRefresh={onRefresh}
+          ListEmptyComponent={<OWEmpty type="nft" label="NO NFTs YET" />}
+          refreshing={refreshing}
+        />
+      </OWBox>
+    </PageWithView>
   );
 });
 
 const styling = (colors) =>
   StyleSheet.create({
-    wrapFlatlist: {
-      flex: 1,
-      paddingVertical: 10,
-    },
-    containerBox: {
-      flex: 1,
-      borderBottomLeftRadius: 0,
-      borderBottomRightRadius: 0,
-      paddingTop: 0,
-    },
     container: {
-      backgroundColor: colors["primary"],
-      borderRadius: spacing["24"],
+      paddingTop: 16,
+      marginTop: 0,
+      backgroundColor: colors["neutral-surface-card"],
+      paddingHorizontal: 16,
     },
-    containerTab: {},
-    title: {
-      ...typography.h3,
-      fontWeight: "700",
-      textAlign: "center",
-      color: colors["primary-text"],
-      marginTop: spacing["12"],
-      marginBottom: spacing["12"],
-    },
-    containerBtn: {
-      backgroundColor: colors["primary-surface-default"],
-      borderRadius: spacing["8"],
-      marginHorizontal: spacing["24"],
-      paddingVertical: spacing["16"],
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: spacing["12"],
-    },
-    flatListItem: {
-      backgroundColor: colors["sub-nft"],
-      borderRadius: spacing["12"],
-      paddingVertical: spacing["8"],
-    },
-    itemPhoto: {
-      width: (metrics.screenWidth - 120) / 2,
-      height: (metrics.screenWidth - 120) / 2,
-      borderRadius: spacing["6"],
-    },
-    containerCollection: {
-      marginHorizontal: spacing["24"],
-      marginTop: spacing["32"],
-    },
-    containerSectionTitle: {
-      flexDirection: "row",
-      marginBottom: spacing["16"],
-    },
-    transactionListEmpty: {
-      justifyContent: "center",
-      alignItems: "center",
-      paddingBottom: 200,
-    },
-    ContainerBtnNft: {
-      margin: 12,
-      marginLeft: 0,
-    },
-    titleNft: {
-      paddingTop: 12,
-    },
-    subTextNft: {
-      textAlign: "justify",
-    },
-    containerImgNft: {
-      borderRadius: 6,
-      width: 128,
-      height: 128,
-    },
-    itemImg: {
-      width: 128,
-      height: 128,
-    },
-    wrapViewNft: {
-      padding: 12,
-      width: 150,
-      height: 222,
-      borderRadius: spacing["12"],
-    },
-    itemText: {
-      ...typography.h7,
-      // color: colors['gray-900'],
-      fontWeight: "700",
+    row: {
+      flex: 1,
+      justifyContent: "space-between",
     },
   });
