4bc8602409dfdc31203dde78bbc1e067882dad99
toan <toan.dq@orai.io>
2024-07-21 08:57:23 +0700

    remove formatjs for android

--- packages/mobile/src/app.tsx
+++ packages/mobile/src/app.tsx
@@ -36,48 +36,6 @@ branch.subscribe({
     console.log("Subscribed to branch successfully!!", params2);
   },
 });
-if (Platform.OS === "android" || typeof HermesInternal !== "undefined") {
-  // https://github.com/web-ridge/react-native-paper-dates/releases/tag/v0.2.15
-
-  // Even though React Native supports the intl on android with "org.webkit:android-jsc-intl:+" option,
-  // it seems that android doesn't support all intl API and this bothers me.
-  // So, to reduce this problem on android, just use the javascript polyfill for intl.
-  require("@formatjs/intl-getcanonicallocales/polyfill");
-  require("@formatjs/intl-locale/polyfill");
-
-  require("@formatjs/intl-pluralrules/polyfill");
-  require("@formatjs/intl-pluralrules/locale-data/en.js");
-
-  require("@formatjs/intl-displaynames/polyfill");
-  require("@formatjs/intl-displaynames/locale-data/en.js");
-
-  // require("@formatjs/intl-listformat/polyfill");
-  // require("@formatjs/intl-listformat/locale-data/en.js");
-
-  require("@formatjs/intl-numberformat/polyfill");
-  require("@formatjs/intl-numberformat/locale-data/en.js");
-
-  require("@formatjs/intl-relativetimeformat/polyfill");
-  require("@formatjs/intl-relativetimeformat/locale-data/en.js");
-
-  require("@formatjs/intl-datetimeformat/polyfill");
-  require("@formatjs/intl-datetimeformat/locale-data/en.js");
-
-  require("@formatjs/intl-datetimeformat/add-golden-tz.js");
-
-  // https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  // const RNLocalize = require("react-native-localize");
-  // if ("__setDefaultTimeZone" in Intl.DateTimeFormat) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // Intl.DateTimeFormat.__setDefaultTimeZone(RNLocalize.getTimeZone());
-  // }
-  // On android, setting the timezone makes that the hour in date looks weird if the hour exceeds 24. Ex) 00:10 AM -> 24:10 AM.
-  // Disable the timezone until finding the solution.
-}
-
-// Prevent native splash screen from autohiding.
 
 // we already log in debugging tools
 LogBox.ignoreAllLogs();
