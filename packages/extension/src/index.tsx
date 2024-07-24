bf63492d0c65ebe8be06d3e5455127d1134eaaa9
toan <toan.dq@orai.io>
2024-07-24 17:13:11 +0700

    update duration

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -334,7 +334,7 @@ ReactDOM.render(
           </ConfirmProvider>
           <ToastContainer
             position="top-center"
-            autoClose={5000}
+            autoClose={3000}
             hideProgressBar={false}
             newestOnTop
             closeOnClick
