8c24b77e20c1c624263cc1baba7384b95fad11dc
toan <toan.dq@orai.io>
2024-08-15 09:06:26 +0700

    add method for mnemonic static

--- packages/crypto/src/mnemonic.ts
+++ packages/crypto/src/mnemonic.ts
@@ -2,7 +2,8 @@
 const bip39 = require("bip39");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const bip32 = require("bip32");
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bs58check = require("bs58check");
 import { Buffer } from "buffer";
 
 export type RNG = <
@@ -66,6 +67,28 @@ export class Mnemonic {
 
     const privateKey = hd.privateKey;
 
+    if (!privateKey) {
+      throw new Error("null hd key");
+    }
+    return privateKey;
+  }
+  static generateMasterSeedFromMnemonic(
+    mnemonic: string,
+    password: string = ""
+  ): Uint8Array {
+    const seed = bip39.mnemonicToSeedSync(mnemonic, password);
+    const masterKey = bip32.fromSeed(seed);
+
+    return Buffer.from(bs58check.decode(masterKey.toBase58()));
+  }
+  static generatePrivateKeyFromMasterSeed(
+    seed: Uint8Array,
+    path: string = `m/44'/118'/0'/0/0`
+  ): Uint8Array {
+    const masterSeed = bip32.fromBase58(bs58check.encode(seed));
+    const hd = masterSeed.derivePath(path);
+
+    const privateKey = hd.privateKey;
     if (!privateKey) {
       throw new Error("null hd key");
     }
