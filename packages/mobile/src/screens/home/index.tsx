90f6950d30d802321e35fbe0921b5cc1b081658e
toan <toan.dq@orai.io>
2024-08-17 22:43:02 +0700

    done logic load balance for mobile

--- packages/mobile/src/screens/home/index.tsx
+++ packages/mobile/src/screens/home/index.tsx
@@ -2,7 +2,9 @@ import React, {
   FunctionComponent,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
+  useState,
   useTransition,
 } from "react";
 import { PageWithScrollViewInBottomTabView } from "../../components/page";
@@ -20,29 +22,53 @@ import { observer } from "mobx-react-lite";
 import { usePrevious } from "../../hooks";
 import { useTheme } from "@src/themes/theme-provider";
 import { useFocusEffect } from "@react-navigation/native";
-import { ChainUpdaterService } from "@owallet/background";
-import { ChainIdEnum, getBase58Address } from "@owallet/common";
+import { ChainInfoWithEmbed, ChainUpdaterService } from "@owallet/background";
+import {
+  addressToPublicKey,
+  API,
+  ChainIdEnum,
+  CWStargate,
+  DenomHelper,
+  getBase58Address,
+  getEvmAddress,
+  getOasisNic,
+  getRpcByChainId,
+  MapChainIdToNetwork,
+  parseRpcBalance,
+} from "@owallet/common";
 import { TokensCardAll } from "./components/tokens-card-all";
 import { AccountBoxAll } from "./components/account-box-new";
 
 import { EarningCardNew } from "./components/earning-card-new";
 import { InjectedProviderUrl } from "../web/config";
 import { useMultipleAssets } from "@src/screens/home/hooks/use-multiple-assets";
-import { IntPretty, PricePretty } from "@owallet/unit";
+import {
+  CoinPretty,
+  Dec,
+  DecUtils,
+  IntPretty,
+  PricePretty,
+} from "@owallet/unit";
 import {
   chainInfos,
   getTokensFromNetwork,
+  network,
   oraichainNetwork,
   TokenItemType,
 } from "@oraichain/oraidex-common";
 import { useCoinGeckoPrices, useLoadTokens } from "@owallet/hooks";
-import { flatten } from "lodash";
+import { debounce, flatten } from "lodash";
 import { showToast } from "@src/utils/helper";
 
 import { MainTabHome } from "./components";
 import { sha256 } from "sha.js";
 import { Mixpanel } from "mixpanel-react-native";
 import { tracking } from "@src/utils/tracking";
+import { ChainInfoInner } from "@owallet/stores";
+import Web3 from "web3";
+import { fromBinary, toBinary } from "@cosmjs/cosmwasm-stargate";
+import { MulticallQueryClient } from "@oraichain/common-contracts-sdk";
+import { ViewToken } from "@src/stores/huge-queries";
 const mixpanel = globalThis.mixpanel as Mixpanel;
 export const HomeScreen: FunctionComponent = observer((props) => {
   const [refreshing, setRefreshing] = React.useState(false);
@@ -64,17 +90,17 @@ export const HomeScreen: FunctionComponent = observer((props) => {
 
   const scrollViewRef = useRef<ScrollView | null>(null);
   const accountOrai = accountStore.getAccount(ChainIdEnum.Oraichain);
-  const { totalPriceBalance, dataTokens, dataTokensByChain, isLoading } =
-    useMultipleAssets(
-      accountStore,
-      priceStore,
-      hugeQueriesStore,
-      chainStore.current.chainId,
-      appInitStore.getInitApp.isAllNetworks,
-      appInitStore,
-      refreshing,
-      accountOrai.bech32Address
-    );
+  // const { totalPriceBalance, dataTokens, dataTokensByChain, isLoading } =
+  //   useMultipleAssets(
+  //     accountStore,
+  //     priceStore,
+  //     hugeQueriesStore,
+  //     chainStore.current.chainId,
+  //     appInitStore.getInitApp.isAllNetworks,
+  //     appInitStore,
+  //     refreshing,
+  //     accountOrai.bech32Address
+  //   );
   const [isPending, startTransition] = useTransition();
   const accountEth = accountStore.getAccount(ChainIdEnum.Ethereum);
   const accountTron = accountStore.getAccount(ChainIdEnum.TRON);
@@ -322,33 +348,470 @@ export const HomeScreen: FunctionComponent = observer((props) => {
   useEffect(() => {
     appInitStore.updatePrices(prices);
   }, [prices]);
-  useEffect(() => {
-    if (!totalPriceBalance || !accountOrai.bech32Address) return;
-    const hashedAddress = new sha256()
-      .update(accountOrai.bech32Address)
-      .digest("hex");
+  // useEffect(() => {
+  //   if (!totalPriceBalance || !accountOrai.bech32Address) return;
+  //   const hashedAddress = new sha256().update(accountOrai.bech32Address).digest("hex");
 
-    const amount = new IntPretty(totalPriceBalance || "0")
-      .maxDecimals(2)
-      .shrink(true)
-      .trim(true)
-      .locale(false)
-      .inequalitySymbol(true);
+  //   const amount = new IntPretty(totalPriceBalance || "0")
+  //     .maxDecimals(2)
+  //     .shrink(true)
+  //     .trim(true)
+  //     .locale(false)
+  //     .inequalitySymbol(true);
 
-    const logEvent = {
-      userId: hashedAddress,
-      totalPrice: amount?.toString() || "0",
-      currency: priceStore.defaultVsCurrency,
-    };
-    if (mixpanel) {
-      mixpanel.track("OWallet - Assets Managements", logEvent);
+  //   const logEvent = {
+  //     userId: hashedAddress,
+  //     totalPrice: amount?.toString() || "0",
+  //     currency: priceStore.defaultVsCurrency
+  //   };
+  //   if (mixpanel) {
+  //     mixpanel.track("OWallet - Assets Managements", logEvent);
+  //   }
+  //   return () => {};
+  // }, [totalPriceBalance, accountOrai.bech32Address, priceStore.defaultVsCurrency]);
+  let pendingUpdates: ViewToken[] = [];
+
+  // Debounced function to apply pending updates
+  const applyPendingUpdates = useCallback(
+    debounce(() => {
+      if (pendingUpdates.length > 0) {
+        setDataBalances((prev) => [...prev, ...pendingUpdates]);
+        pendingUpdates = [];
+      }
+    }, 75),
+    []
+  ); // Adjust the delay as needed
+
+  // Function to add new balances to the pending updates
+  const updateDataBalances = (newBalances: ViewToken[]) => {
+    pendingUpdates = [...pendingUpdates, ...newBalances];
+    applyPendingUpdates();
+  };
+  const [dataBalances, setDataBalances] = useState<ViewToken[]>([]);
+  const availableTotalPrice = useMemo(() => {
+    let result: PricePretty | undefined;
+    for (const bal of dataBalances) {
+      if (bal.price) {
+        if (!result) {
+          result = bal.price;
+        } else {
+          result = result.add(bal.price);
+        }
+      }
     }
+    return result;
+  }, [dataBalances]);
+  useEffect(() => {
+    setDataBalances([]);
+    pendingUpdates = [];
+    setTimeout(() => {
+      const allChain = Array.from(hugeQueriesStore.getAllChainMap.values());
+      for (const { address, chainInfo } of allChain) {
+        if (!address) continue;
+
+        switch (chainInfo.networkType) {
+          case "cosmos":
+            if (chainInfo.chainId === ChainIdEnum.Oraichain) {
+              getBalanceCW20Oraichain(address, chainInfo);
+            }
+            getBalanceNativeCosmos(address, chainInfo);
+            break;
+          case "evm":
+            getBalanceNativeEvm(address, chainInfo);
+            if (
+              chainInfo.chainId === ChainIdEnum.BNBChain ||
+              chainInfo.chainId === ChainIdEnum.Ethereum
+            ) {
+              getBalancesErc20(address, chainInfo);
+            } else if (chainInfo.chainId === ChainIdEnum.TRON) {
+              getBalancessTrc20(address, chainInfo);
+            }
+            break;
+          case "bitcoin":
+            const legacyAddress = accountStore.getAccount(
+              ChainIdEnum.Bitcoin
+            ).legacyAddress;
+            getBalanceBtc(address, chainInfo);
+            if (!legacyAddress) break;
+            getBalanceBtc(legacyAddress, chainInfo);
+            break;
+          default:
+            break;
+        }
+        console.log(address, "chain");
+      }
+    }, 500);
     return () => {};
-  }, [
-    totalPriceBalance,
-    accountOrai.bech32Address,
-    priceStore.defaultVsCurrency,
-  ]);
+  }, [accountOrai.bech32Address]);
+  const getBalanceBtc = async (
+    address,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+    // type: AddressBtcType
+  ) => {
+    const data = await API.getBtcBalance({
+      address,
+      baseUrl: chainInfo.rest,
+    });
+    if (data) {
+      const totalBtc = data.reduce((acc, curr) => acc + curr.value, 0);
+      // pushTokenQueue(chainInfo.stakeCurrency, totalBtc, chainInfo, type);
+      if (totalBtc) {
+        const token = new CoinPretty(chainInfo.stakeCurrency, totalBtc);
+        updateDataBalances([
+          {
+            token,
+            price: priceStore.calculatePrice(token),
+            chainInfo,
+            isFetching: false,
+            error: null,
+          },
+        ]);
+      }
+    }
+  };
+  const getBalanceNativeCosmos = async (
+    address: string,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+  ) => {
+    const { balances } = await API.getAllBalancesNativeCosmos({
+      address,
+      baseUrl: chainInfo.rest,
+    });
+
+    const balanceObj = balances.reduce((obj, { denom, amount }) => {
+      obj[denom] = amount;
+      return obj;
+    }, {});
+
+    const allTokensAddress: string[] = [];
+    const newDataBalances = [];
+
+    balances.forEach(({ denom, amount }) => {
+      const currency = chainInfo.currencyMap.get(denom);
+
+      if (currency) {
+        const token = new CoinPretty(currency, amount);
+        newDataBalances.push({
+          token,
+          price: priceStore.calculatePrice(token),
+          chainInfo,
+          isFetching: false,
+          error: null,
+        });
+      } else if (MapChainIdToNetwork[chainInfo.chainId]) {
+        const str = `${
+          MapChainIdToNetwork[chainInfo.chainId]
+        }%2B${new URLSearchParams(denom).toString().replace("=", "")}`;
+        allTokensAddress.push(str);
+      }
+    });
+    updateDataBalances(newDataBalances);
+
+    if (allTokensAddress.length > 0) {
+      const tokenInfos = await API.getMultipleTokenInfo({
+        tokenAddresses: allTokensAddress.join(","),
+      });
+
+      const newCurrencies = tokenInfos
+        .map((tokeninfo) => {
+          const existingToken = chainInfo.currencies.find(
+            (item) =>
+              item.coinDenom?.toUpperCase() === tokeninfo.abbr?.toUpperCase()
+          );
+
+          if (!existingToken) {
+            const infoToken = {
+              coinImageUrl: tokeninfo.imgUrl,
+              coinDenom: tokeninfo.abbr,
+              coinGeckoId: tokeninfo.coingeckoId,
+              coinDecimals: tokeninfo.decimal,
+              coinMinimalDenom: tokeninfo.denom,
+            };
+
+            const token = new CoinPretty(
+              infoToken,
+              balanceObj[tokeninfo.denom]
+            );
+            newDataBalances.push({
+              token,
+              price: priceStore.calculatePrice(token),
+              chainInfo,
+              isFetching: false,
+              error: null,
+            });
+
+            return infoToken;
+          }
+          return null;
+        })
+        .filter(Boolean);
+      updateDataBalances(newDataBalances);
+
+      if (newCurrencies.length > 0) {
+        chainInfo.addCurrencies(...newCurrencies);
+      }
+    }
+  };
+
+  const getBalanceCW20Oraichain = async (address, chainInfo) => {
+    const data = toBinary({ balance: { address } });
+
+    try {
+      const account = accountStore.getAccount(ChainIdEnum.Oraichain);
+      const client = await CWStargate.init(
+        account,
+        ChainIdEnum.Oraichain,
+        chainInfo.rpc
+      );
+
+      const tokensCw20 = chainInfo.currencies.filter(
+        (item) => new DenomHelper(item.coinMinimalDenom).contractAddress
+      );
+
+      const multicall = new MulticallQueryClient(client, network.multicall);
+      const res = await multicall.aggregate({
+        queries: tokensCw20.map((t) => ({
+          address: new DenomHelper(t.coinMinimalDenom).contractAddress,
+          data,
+        })),
+      });
+
+      const newDataBalances = res.return_data.reduce(
+        (acc, { success, data }, index) => {
+          if (success) {
+            const balanceRes = fromBinary(data);
+            const currency = chainInfo.currencyMap.get(
+              tokensCw20[index].coinMinimalDenom
+            );
+
+            if (currency) {
+              const token = new CoinPretty(currency, balanceRes.balance);
+              acc.push({
+                token,
+                price: priceStore.calculatePrice(token),
+                chainInfo,
+                isFetching: false,
+                error: null,
+              });
+            }
+          }
+          return acc;
+        },
+        []
+      );
+      updateDataBalances(newDataBalances);
+    } catch (error) {
+      console.error("Error fetching CW20 balance:", error);
+    }
+  };
+  const getBalancesErc20 = async (
+    address: string,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+  ) => {
+    try {
+      const network = MapChainIdToNetwork[chainInfo.chainId];
+
+      const res = await API.getAllBalancesEvm({ address, network });
+
+      const balances = res?.result || [];
+      if (balances.length === 0) return;
+
+      const tokenAddresses = balances
+        .map(({ tokenAddress }) => `${network}%2B${tokenAddress}`)
+        .join(",");
+      const tokenInfos = await API.getMultipleTokenInfo({ tokenAddresses });
+
+      const existingCurrencies = new Set(
+        chainInfo.currencies.map((currency) =>
+          currency.coinDenom?.toUpperCase()
+        )
+      );
+
+      const newCurrencies = tokenInfos
+        .filter(
+          (tokenInfo, index) =>
+            !existingCurrencies.has(tokenInfo.abbr?.toUpperCase()) &&
+            tokenInfos.findIndex(
+              (c) => c.contractAddress === tokenInfo.contractAddress
+            ) === index
+        )
+        .map((tokenInfo) => ({
+          coinImageUrl: tokenInfo.imgUrl,
+          coinDenom: tokenInfo.abbr,
+          coinGeckoId: tokenInfo.coingeckoId,
+          coinDecimals: tokenInfo.decimal,
+          coinMinimalDenom: `erc20:${tokenInfo.contractAddress}:${tokenInfo.name}`,
+          contractAddress: tokenInfo.contractAddress,
+        }));
+
+      if (newCurrencies.length === 0) return;
+      // console.log(newCurrencies, "newCurrencies");
+      chainInfo.addCurrencies(...newCurrencies);
+
+      const newDataBalances = chainInfo.currencies
+        .map((item) => {
+          const balance = balances.find(
+            (balance) =>
+              balance.tokenAddress.toLowerCase() ===
+              new DenomHelper(
+                item.coinMinimalDenom
+              )?.contractAddress.toLowerCase()
+          );
+          if (!balance) return undefined;
+          const token = new CoinPretty(
+            item,
+            new Dec(balance.balance).mul(
+              DecUtils.getTenExponentN(item.coinDecimals)
+            )
+          );
+
+          return {
+            token,
+            price: priceStore.calculatePrice(token),
+            chainInfo,
+            isFetching: false,
+            error: null,
+          };
+        })
+        .filter((balance) => balance !== undefined);
+
+      if (newDataBalances.length > 0) {
+        updateDataBalances(newDataBalances);
+      }
+    } catch (error) {
+      console.error("Error fetching ERC-20 balances:", error);
+    }
+  };
+  const getBalanceNativeEvm = async (
+    address: string,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+  ) => {
+    try {
+      if (chainInfo.chainId === ChainIdEnum.Oasis) {
+        getBalanceOasis(address, chainInfo);
+        return;
+      } else if (chainInfo.chainId === ChainIdEnum.KawaiiEvm) {
+        return;
+      }
+      const web3 = new Web3(getRpcByChainId(chainInfo, chainInfo.chainId));
+      const ethBalance = await web3.eth.getBalance(address);
+      const token = new CoinPretty(chainInfo.stakeCurrency, ethBalance);
+      updateDataBalances([
+        {
+          token,
+          price: priceStore.calculatePrice(token),
+          chainInfo,
+          isFetching: false,
+          error: null,
+        },
+      ]);
+    } catch (error) {
+      console.log(error, chainInfo.chainName, "error native evm");
+    }
+  };
+  const getBalancessTrc20 = async (
+    address: string,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+  ) => {
+    try {
+      const res = await API.getAllBalancesEvm({
+        address: getBase58Address(address),
+        network: MapChainIdToNetwork[chainInfo.chainId],
+      });
+
+      if (!res?.trc20) return;
+      const tokenAddresses = res?.trc20
+        .map((item, index) => {
+          return `${MapChainIdToNetwork[chainInfo.chainId]}%2B${
+            Object.keys(item)[0]
+          }`;
+        })
+        .join(",");
+      const tokenInfos = await API.getMultipleTokenInfo({ tokenAddresses });
+
+      const existingCurrencies = new Set(
+        chainInfo.currencies.map((currency) =>
+          currency.coinDenom?.toUpperCase()
+        )
+      );
+
+      const newCurrencies = tokenInfos
+        .filter(
+          (tokenInfo, index) =>
+            !existingCurrencies.has(tokenInfo.abbr?.toUpperCase()) &&
+            tokenInfos.findIndex(
+              (c) => c.contractAddress === tokenInfo.contractAddress
+            ) === index
+        )
+        .map((tokenInfo) => ({
+          coinImageUrl: tokenInfo.imgUrl,
+          coinDenom: tokenInfo.abbr,
+          coinGeckoId: tokenInfo.coingeckoId,
+          coinDecimals: tokenInfo.decimal,
+          coinMinimalDenom: `erc20:${getEvmAddress(
+            tokenInfo.contractAddress
+          )}:${tokenInfo.name}`,
+          contractAddress: tokenInfo.contractAddress,
+        }));
+      chainInfo.addCurrencies(...newCurrencies);
+      const newDataBalances = chainInfo.currencies
+        .map((item) => {
+          const contract = res.trc20.find(
+            (obj) =>
+              Object.keys(obj)[0] ===
+              getBase58Address(
+                new DenomHelper(item.coinMinimalDenom)?.contractAddress
+              )
+          );
+
+          if (!contract) return undefined;
+
+          const token = new CoinPretty(
+            item,
+            Number(Object.values(contract)[0])
+          );
+
+          return {
+            token,
+            price: priceStore.calculatePrice(token),
+            chainInfo,
+            isFetching: false,
+            error: null,
+          };
+        })
+        .filter((balance) => balance !== undefined);
+
+      if (newDataBalances.length > 0) {
+        updateDataBalances(newDataBalances);
+      }
+    } catch (e) {
+      console.log(e, "err get Trc20 balances");
+    }
+  };
+  const getBalanceOasis = async (
+    address: string,
+    chainInfo: ChainInfoInner<ChainInfoWithEmbed>
+  ) => {
+    const nic = getOasisNic(chainInfo.raw.grpc);
+    const publicKey = await addressToPublicKey(address);
+    const account = await nic.stakingAccount({ owner: publicKey, height: 0 });
+    const grpcBalance = parseRpcBalance(account);
+    if (grpcBalance) {
+      const token = new CoinPretty(
+        chainInfo.stakeCurrency,
+        grpcBalance.available
+      );
+      updateDataBalances([
+        {
+          token,
+          price: priceStore.calculatePrice(token),
+          chainInfo,
+          isFetching: false,
+          error: null,
+        },
+      ]);
+    }
+  };
   return (
     <PageWithScrollViewInBottomTabView
       refreshControl={
@@ -368,18 +831,12 @@ export const HomeScreen: FunctionComponent = observer((props) => {
       ref={scrollViewRef}
     >
       <AccountBoxAll
-        isLoading={isLoading}
-        totalBalanceByChain={new PricePretty(
-          fiatCurrency,
-          dataTokensByChain?.[chainStore.current.chainId]?.totalBalance
-        )?.toString()}
-        totalPriceBalance={new PricePretty(
-          fiatCurrency,
-          totalPriceBalance
-        )?.toString()}
+        isLoading={false}
+        totalBalanceByChain={"0"}
+        totalPriceBalance={availableTotalPrice?.toString()}
       />
-      <EarningCardNew />
-      <MainTabHome dataTokens={dataTokens} />
+      {/* <EarningCardNew /> */}
+      <MainTabHome dataTokens={dataBalances} />
     </PageWithScrollViewInBottomTabView>
   );
 });
