6c616275d5866fed28ac55e6709697e4d69200d8
sonlha <lhas1307@gmail.com>
2024-08-26 17:39:22 +0700

    _onPressCompound - more modal

--- packages/mobile/src/screens/home/components/more-modal.tsx
+++ packages/mobile/src/screens/home/components/more-modal.tsx
@@ -5,19 +5,88 @@ import OWIcon from "@src/components/ow-icon/ow-icon";
 import { BottomSheetProps } from "@gorhom/bottom-sheet";
 import { registerModal } from "@src/modals/base";
 import { useTheme } from "@src/themes/theme-provider";
+import { navigate } from "@src/router/root";
+import { SCREENS } from "@src/common/constants";
+import { showToast } from "@src/utils/helper";
+import { useStore } from "@src/stores";
+import { tracking } from "@src/utils/tracking";
 
 const MoreModal: FunctionComponent<{
-  copyable?: boolean;
   onPress?: Function;
   isOpen: boolean;
   close: () => void;
   bottomSheetModalConfig?: Omit<BottomSheetProps, "snapPoints" | "children">;
-}> = registerModal(({ onPress, copyable = true, close }) => {
+}> = registerModal(({ onPress, close }) => {
   const { colors } = useTheme();
+  const { chainStore, accountStore, queriesStore, analyticsStore } = useStore();
+
+  const chainId = chainStore.current.chainId;
+
+  const _onPressCompound = async () => {
+    const queries = queriesStore.get(chainId);
+    const account = accountStore.getAccount(chainId);
+    const queryReward = queries.cosmos.queryRewards.getQueryBech32Address(
+      account.bech32Address
+    );
+    const stakingReward = queryReward.stakableReward;
+
+    try {
+      const validatorRewars = [];
+      queryReward
+        .getDescendingPendingRewardValidatorAddresses(10)
+        .map((validatorAddress) => {
+          const rewards = queries.cosmos.queryRewards
+            .getQueryBech32Address(account.bech32Address)
+            .getStakableRewardOf(validatorAddress);
+          validatorRewars.push({ validatorAddress, rewards });
+        });
+
+      if (queryReward) {
+        tracking(`${chainStore.current.chainName} Compound`);
+        await account.cosmos.sendWithdrawAndDelegationRewardMsgs(
+          queryReward.getDescendingPendingRewardValidatorAddresses(10),
+          validatorRewars,
+          "",
+          {},
+          {},
+          {
+            onBroadcasted: (txHash) => {
+              analyticsStore.logEvent("Compound reward tx broadcasted", {
+                chainId: chainId,
+                chainName: chainStore.current.chainName,
+              });
+            },
+          },
+          stakingReward.currency.coinMinimalDenom
+        );
+      } else {
+        showToast({
+          message: "There is no reward!",
+          type: "danger",
+        });
+      }
+    } catch (e) {
+      console.error({ errorClaim: e });
+      if (!e?.message?.startsWith("Transaction Rejected")) {
+        showToast({
+          message:
+            e?.message ?? "Something went wrong! Please try again later.",
+          type: "danger",
+        });
+        return;
+      }
+    }
+  };
 
   return (
     <View style={{ padding: 24 }}>
       <TouchableOpacity
+        onPress={() => {
+          _onPressCompound();
+          setTimeout(() => {
+            close();
+          }, 1000);
+        }}
         style={{
           flexDirection: "row",
           alignItems: "center",
@@ -50,6 +119,13 @@ const MoreModal: FunctionComponent<{
         </View>
       </TouchableOpacity>
       <TouchableOpacity
+        onPress={() => {
+          close();
+          navigate(SCREENS.STACK.Others, {
+            screen: SCREENS.BuyFiat,
+          });
+          return;
+        }}
         style={{
           flexDirection: "row",
           alignItems: "center",
