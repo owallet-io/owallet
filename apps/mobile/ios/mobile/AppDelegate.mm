5b8572766d0795c2afa0b02f43ee5cbbf52da33c
toan <toan.dq@orai.io>
2024-08-26 12:09:37 +0700

    add deeplink for ios

--- apps/mobile/ios/mobile/AppDelegate.mm
+++ apps/mobile/ios/mobile/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <React/RCTLinkingManager.h>
 #import <React/RCTBundleURLProvider.h>
 #import <Firebase.h>
 #import <CodePush/CodePush.h>
@@ -8,6 +9,21 @@
 #import <AppCenterReactNativeCrashes.h>
 #import <RNBranch/RNBranch.h>
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
