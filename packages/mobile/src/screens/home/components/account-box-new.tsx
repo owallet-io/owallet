802154c39c4711901c0c0a58caf12340dae38c09
sonlha <lhas1307@gmail.com>
2024-08-29 16:55:31 +0700

    update colors code

--- packages/mobile/src/screens/home/components/account-box-new.tsx
+++ packages/mobile/src/screens/home/components/account-box-new.tsx
@@ -42,28 +42,24 @@ import MoreModal from "./more-modal";
 
 const widthAndHeight = 100;
 const colorList = [
-  "#ff6c00",
-  "#ff3c00",
-  "#945EF8",
-  "#C170FF",
-  "#00AD26",
-  "#ECFEEE",
-  "#ffb300",
-  "#ff9100",
-  "#8B1BFB",
-  "#FF5947",
-  "#39DD47",
-  "#D3FDD7",
-  "#FFE1AD",
-  "#B87500",
-  "#F29900",
+  "#81ACEB",
+  "#FFEA28",
+  "#9ED275",
+  "#9AB66B",
+  "#F07895",
   "#9D81EB",
-  "#7A4D00",
-  "#E01600",
-  "#CAEB60",
-  "#fbd203",
-  "#A81100",
-  "#FFEDEB",
+  "#F7931A",
+  "#627EEA",
+  "#E9B6E6",
+  "#494949",
+  "#CFE389",
+  "#93C067",
+  "#3E7C55",
+  "#29575D",
+  "#649A57",
+  "#CF72B9",
+  "#854EAF",
+  "#5D30A5",
 ];
 
 const randomColors = shuffleArray(colorList);
@@ -182,13 +178,13 @@ export const AccountBoxAll: FunctionComponent<{
       });
 
       tmpChain.push({
-        color: "#F5F5F7",
+        color: "#494949",
         totalBalance: otherValue,
         name: "Other",
       });
       setChainListWithBalance(tmpChain);
       setSeries([...tmpSeries, otherValue]);
-      setSliceColor([...tmpSliceColor, "#F5F5F7"]);
+      setSliceColor([...tmpSliceColor, "#494949"]);
     }, [dataBalances, accountOrai.bech32Address]);
 
     const { isTimedOut, setTimer } = useSimpleTimer();
