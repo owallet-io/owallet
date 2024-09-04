d6eebeb4674c9fcb31f175298c5c286425a89f4a
sonlha <lhas1307@gmail.com>
2024-09-04 17:40:34 +0700

    update stake component extension

--- packages/extension/src/pages/home/stake.tsx
+++ packages/extension/src/pages/home/stake.tsx
@@ -1,11 +1,16 @@
-import React, { FunctionComponent, useMemo, useRef, useState } from "react";
+import React, {
+  FunctionComponent,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Tooltip } from "reactstrap";
 import { useStore } from "../../stores";
 import { observer } from "mobx-react-lite";
 import styleStake from "./stake.module.scss";
 import classnames from "classnames";
 import { Dec } from "@owallet/unit";
-
 import { useHistory } from "react-router";
 import { FormattedMessage } from "react-intl";
 import { ChainIdEnum } from "@owallet/common";
@@ -13,6 +18,120 @@ import { Button } from "components/common/button";
 import { Text } from "components/common/text";
 import colors from "theme/colors";
 import { toast } from "react-toastify";
+import axios from "axios";
+import moment from "moment";
+
+const owalletOraichainAddress =
+  "oraivaloper1q53ujvvrcd0t543dsh5445lu6ar0qr2zv4yhhp";
+const owalletOsmosisAddress =
+  "osmovaloper1zqevmn000unnjj709akc8p86f9jc4xevf8f8g3";
+
+const valVotingPower = 1000;
+const daysInYears = 365.2425;
+
+async function getBlockTime(lcdEndpoint, blockHeightDiff = 100) {
+  try {
+    // Fetch the latest block
+    const latestBlockResponse = await fetch(
+      `${lcdEndpoint}/cosmos/base/tendermint/v1beta1/blocks/latest`
+    );
+    const latestBlockData = await latestBlockResponse.json();
+
+    if (!latestBlockResponse.ok || !latestBlockData.block) {
+      throw new Error("Failed to fetch latest block.");
+    }
+
+    // Get latest block height and timestamp
+    const latestBlockHeight = parseInt(latestBlockData.block.header.height);
+    const latestBlockTime = new Date(
+      latestBlockData.block.header.time
+    ).getTime();
+
+    // Fetch a previous block
+    const previousBlockHeight = latestBlockHeight - blockHeightDiff;
+    const previousBlockResponse = await fetch(
+      `${lcdEndpoint}/cosmos/base/tendermint/v1beta1/blocks/${previousBlockHeight}`
+    );
+    const previousBlockData = await previousBlockResponse.json();
+
+    if (!previousBlockResponse.ok || !previousBlockData.block) {
+      throw new Error("Failed to fetch previous block.");
+    }
+
+    // Get previous block timestamp
+    const previousBlockTime = new Date(
+      previousBlockData.block.header.time
+    ).getTime();
+
+    // Calculate average block time
+    const timeDiff = (latestBlockTime - previousBlockTime) / blockHeightDiff; // Time difference divided by number of blocks
+    const averageBlockTimeInSeconds = timeDiff / 1000; // Convert milliseconds to seconds
+
+    return averageBlockTimeInSeconds;
+  } catch (error) {
+    console.error("Error fetching block time:", error);
+    return 0;
+  }
+}
+
+async function getParamsFromLcd(lcdEndpoint) {
+  try {
+    const params = await axios.get(
+      `${lcdEndpoint}/cosmos/distribution/v1beta1/params`
+    );
+    return params?.data?.params;
+  } catch (error) {
+    console.error(
+      "Error fetching validator getParamsFromLcd:",
+      `${lcdEndpoint}/cosmos/distribution/v1beta1/params`,
+      error
+    );
+    return {};
+  }
+}
+
+async function getTotalSupply(chainInfo) {
+  try {
+    if (chainInfo.stakeCurrency.coinMinimalDenom === "orai") {
+      const totalSupply = await axios.get(
+        `${chainInfo.rest}/cosmos/bank/v1beta1/supply`
+      );
+      const supply = totalSupply.data.supply?.find((s) => s.denom === "orai");
+
+      return supply?.amount;
+    } else {
+      const totalSupply = await axios.get(
+        `${chainInfo.rest}/cosmos/bank/v1beta1/supply/by_denom?denom=${chainInfo.stakeCurrency.coinMinimalDenom}`
+      );
+
+      return totalSupply?.data?.amount.amount;
+    }
+  } catch (error) {
+    console.error(
+      "Error fetching validator getTotalSupply:",
+      `${chainInfo.rest}/cosmos/bank/v1beta1/supply`,
+      error
+    );
+    return 0;
+  }
+}
+
+async function getInflationRate(lcdEndpoint) {
+  try {
+    const inflation = await axios.get(
+      `${lcdEndpoint}/cosmos/mint/v1beta1/inflation`
+    );
+
+    return inflation?.data?.inflation;
+  } catch (error) {
+    console.error(
+      "Error fetching validator getInflationRate:",
+      `${lcdEndpoint}/cosmos/mint/v1beta1/inflation`,
+      error
+    );
+    return 0;
+  }
+}
 
 export const StakeView: FunctionComponent = observer(() => {
   const history = useHistory();
@@ -184,6 +303,134 @@ export const LinkStakeView: FunctionComponent = observer(() => {
     return stakable?.balance.toDec().gt(new Dec(0));
   }, [stakable?.balance]);
 
+  const [owalletOraichain, setOwalletOraichain] = useState("0");
+  const [owalletOsmosis, setOwalletOsmosis] = useState("0");
+
+  const calculateAPRByChain = async (chainInfo, validatorAddress) => {
+    try {
+      const totalSupply = await getTotalSupply(chainInfo);
+      const { community_tax, base_proposer_reward, bonus_proposer_reward } =
+        await getParamsFromLcd(chainInfo.rest);
+      const inflationRate = await getInflationRate(chainInfo.rest);
+      const res = await axios.get(
+        `${chainInfo.rest}/cosmos/staking/v1beta1/validators/${validatorAddress}`
+      );
+      const validator = res?.data.validator;
+
+      const blockTime = await getBlockTime(chainInfo.rest);
+
+      const blocksPerYear = (60 * 60 * 24 * 365) / blockTime;
+
+      let votingPower = valVotingPower;
+      const totalDelegatedTokens = parseFloat(validator.tokens);
+
+      if (totalDelegatedTokens > 0) {
+        votingPower = totalDelegatedTokens;
+      }
+
+      const blockProvision =
+        (parseFloat(inflationRate) * totalSupply) / blocksPerYear;
+      const voteMultiplier =
+        1 - community_tax - base_proposer_reward - bonus_proposer_reward;
+      const valRewardPerBlock =
+        votingPower > 0
+          ? ((blockProvision * votingPower) / votingPower) * voteMultiplier
+          : 0;
+
+      const delegatorsRewardPerBlock =
+        valRewardPerBlock * (1 - validator.commission.commission_rates.rate);
+      const numBlocksPerDay = blockTime > 0 ? (60 / blockTime) * 60 * 24 : 0;
+
+      const delegatorsRewardPerDay = delegatorsRewardPerBlock * numBlocksPerDay;
+      const apr = (delegatorsRewardPerDay * daysInYears) / totalDelegatedTokens;
+
+      return apr;
+    } catch (err) {
+      console.log("error calculateAPRByChain", err);
+      return 0;
+    }
+  };
+
+  const getOWalletOraichainAPR = async () => {
+    const chainInfo = chainStore.getChain(ChainIdEnum.Oraichain);
+    const apr = await calculateAPRByChain(chainInfo, owalletOraichainAddress);
+    setOwalletOraichain(apr.toFixed(2));
+  };
+
+  const getOWalletOsmosisAPR = async () => {
+    const currentDate = moment();
+    // Subtract 10 days from the current date
+    const pastDate = currentDate.subtract(10, "days");
+
+    try {
+      const params = await axios.get(
+        `https://www.datalenses.zone/numia/osmosis/lenses/apr?start_date=${pastDate.format(
+          "YYYY-MM-DD"
+        )}&end_date=${moment().format("YYYY-MM-DD")}`
+      );
+
+      if (params?.data?.length > 0) {
+        setOwalletOsmosis(params.data[0].total?.toFixed(2));
+      }
+    } catch (error) {
+      console.error(
+        "Error fgetOWalletOsmosisAPR:",
+        `https://www.datalenses.zone/numia/osmosis/lenses/apr?start_date=${pastDate.format(
+          "YYYY-MM-DD"
+        )}&end_date=${moment().format("YYYY-MM-DD")}`,
+        error
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (chainStore.current.chainId === ChainIdEnum.Oraichain) {
+      getOWalletOraichainAPR();
+    }
+
+    if (chainStore.current.chainId === ChainIdEnum.Osmosis) {
+      getOWalletOsmosisAPR();
+    }
+  }, [chainStore.current.chainId]);
+
+  const renderAPR = () => {
+    if (
+      chainStore.current.chainId === ChainIdEnum.Oraichain ||
+      chainStore.current.chainId === ChainIdEnum.Osmosis
+    ) {
+      return (
+        <p
+          className={classnames(
+            "h4",
+            "my-0",
+            "font-weight-normal",
+            styleStake.paragraphSub
+          )}
+        >
+          <Text>
+            <FormattedMessage
+              id="main.stake.message.earning"
+              values={{
+                apr: (
+                  <React.Fragment>
+                    {chainStore.current.chainId === ChainIdEnum.Oraichain
+                      ? owalletOraichain
+                      : owalletOsmosis}
+                    {inflation.isFetching ? (
+                      <span>
+                        <i className="fas fa-spinner fa-spin" />
+                      </span>
+                    ) : null}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          </Text>
+        </p>
+      );
+    }
+  };
+
   return (
     <div
       style={{
@@ -194,58 +441,25 @@ export const LinkStakeView: FunctionComponent = observer(() => {
         paddingTop: 8,
       }}
     >
-      <div className={styleStake.vertical}>
-        {inflation.inflation.toDec().equals(new Dec(0)) ? null : (
-          <p
-            className={classnames(
-              "h4",
-              "my-0",
-              "font-weight-normal",
-              styleStake.paragraphSub
-            )}
-          >
-            <Text>
-              <FormattedMessage
-                id="main.stake.message.earning"
-                values={{
-                  apr: (
-                    <React.Fragment>
-                      {inflation.inflation.trim(true).maxDecimals(2).toString()}
-                      {inflation.isFetching ? (
-                        <span>
-                          <i className="fas fa-spinner fa-spin" />
-                        </span>
-                      ) : null}
-                    </React.Fragment>
-                  ),
-                }}
-              />
-            </Text>
-          </p>
-        )}
-      </div>
+      <div className={styleStake.vertical}>{renderAPR()}</div>
       <div style={{ flex: 1 }} />
       <div
         onClick={(e) => {
-          const pttrn = /^(https?:\/\/)?(www\.)?([^\/]+)/gm;
-          const urlInfo = pttrn.exec(chainInfo.raw.txExplorer.txUrl);
-          window.open(
-            urlInfo && urlInfo[0] ? urlInfo[0] : "https://scan.orai.io/"
-          );
-          // if (!isStakableExist) {
-          //   e.preventDefault();
-          // } else {
-          //   // history.push('/stake/validator-list');
-          //   // analyticsStore.logEvent('Stake button clicked', {
-          //   //   chainId: chainId,
-          //   //   chainName: chainStore.current.chainName
-          //   // });
-          //   const pttrn = /^(https?:\/\/)?(www\.)?([^\/]+)/gm;
-          //   const urlInfo = pttrn.exec(chainInfo.raw.txExplorer.txUrl);
-          //   window.open(
-          //     urlInfo && urlInfo[0] ? urlInfo[0] : 'https://scan.orai.io/'
-          //   );
-          // }
+          if (chainId === ChainIdEnum.Oraichain) {
+            window.open(
+              "https://scanium.io/Oraichain/staking/oraivaloper1q53ujvvrcd0t543dsh5445lu6ar0qr2zv4yhhp"
+            );
+          } else if (chainId === ChainIdEnum.Osmosis) {
+            window.open(
+              "https://www.mintscan.io/osmosis/validators/osmovaloper1zqevmn000unnjj709akc8p86f9jc4xevf8f8g3"
+            );
+          } else {
+            const pttrn = /^(https?:\/\/)?(www\.)?([^\/]+)/gm;
+            const urlInfo = pttrn.exec(chainInfo.raw.txExplorer.txUrl);
+            window.open(
+              urlInfo && urlInfo[0] ? urlInfo[0] : "https://scanium.io//"
+            );
+          }
         }}
       >
         <span
