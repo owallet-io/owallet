f17ed5c8f4600127874b33c374c0b5a432e2839e
toan <toan.dq@orai.io>
2024-09-17 15:16:54 +0700

    Fix: add verify screen when create wallet

--- packages/mobile/src/navigation.tsx
+++ packages/mobile/src/navigation.tsx
@@ -224,8 +224,9 @@ export const AppNavigation: FunctionComponent = observer(() => {
               name={SCREENS.RegisterMain}
               component={NewMnemonicScreen}
             />
+
             <Stack.Screen
-              name={SCREENS.RegisterVerifyMnemonicMain}
+              name={SCREENS.RegisterVerifyMnemonic}
               component={VerifyMnemonicScreen}
             />
             <Stack.Screen
