52f8f55b0527289f87824b35365123dbd2fd3ec5
toan <toan.dq@orai.io>
2024-09-27 10:32:04 +0700

    Add: config meme coin

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -142,6 +142,33 @@ export const EmbedChainInfos: AppChainInfo[] = [
             "https://assets.coingecko.com/coins/images/17980/standard/ton_symbol.png?1696517498",
         },
         {
+          coinDenom: "PEPE",
+          coinMinimalDenom:
+            "factory/orai1wuvhex9xqs3r539mvc6mtm7n20fcj3qr2m0y9khx6n5vtlngfzes3k0rq9/extPEPE",
+          coinDecimals: 6,
+          coinGeckoId: "pepe",
+          coinImageUrl:
+            "https://assets.coingecko.com/coins/images/29850/standard/pepe-token.jpeg?1696528776",
+        },
+        {
+          coinDenom: "HMSTR",
+          coinMinimalDenom:
+            "factory/orai1wuvhex9xqs3r539mvc6mtm7n20fcj3qr2m0y9khx6n5vtlngfzes3k0rq9/HMSTR",
+          coinDecimals: 9,
+          coinGeckoId: "hamster-kombat",
+          coinImageUrl:
+            "https://assets.coingecko.com/coins/images/39102/standard/hamster-removebg-preview.png?1720514486",
+        },
+        {
+          coinDenom: "CAT",
+          coinMinimalDenom:
+            "factory/orai1wuvhex9xqs3r539mvc6mtm7n20fcj3qr2m0y9khx6n5vtlngfzes3k0rq9/extCAT",
+          coinDecimals: 6,
+          coinGeckoId: "simon-s-cat",
+          coinImageUrl:
+            "https://assets.coingecko.com/coins/images/39765/standard/Simon's_Cat_Logo.png?1724017505",
+        },
+        {
           type: "cw20",
           coinDenom: "SCORAI",
           coinMinimalDenom:
