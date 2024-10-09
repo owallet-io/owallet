0350c78c4be2d2203bcf3d6d183af7ba921a3dc6
toan <toan.dq@orai.io>
2024-10-09 13:41:46 +0700

    fix ui mobile done screen

--- apps/mobile/src/screens/stake/validator-list/new-list.tsx
+++ apps/mobile/src/screens/stake/validator-list/new-list.tsx
@@ -209,36 +209,6 @@ export const ValidatorList: FunctionComponent = observer(() => {
           />
         </View>
       </View>
-      {chainStore.current.chainId !== ChainIdEnum.Oraichain ? (
-        <TouchableOpacity
-          style={{
-            flexDirection: "row",
-            borderRadius: 999,
-            width: metrics.screenWidth / 3,
-            alignItems: "center",
-            paddingHorizontal: 12,
-            justifyContent: "space-between",
-            alignSelf: "flex-end",
-            paddingTop: 16,
-          }}
-          onPress={() => {
-            if (sort === "Voting Power") {
-              setSort("Voting Power Increase");
-            } else {
-              setSort("Voting Power");
-            }
-          }}
-        >
-          <OWText weight="600">{"Voting Power"}</OWText>
-          <View>
-            <OWIcon
-              color={colors["neutral-icon-on-light"]}
-              name="double-arrow"
-              size={18}
-            />
-          </View>
-        </TouchableOpacity>
-      ) : null}
       {dataShuffle?.length <= 0 ? <OwEmpty /> : null}
       {dataShuffle.map((item, index) => renderItem({ item, index }))}
     </View>
