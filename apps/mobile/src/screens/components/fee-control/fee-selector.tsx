868e2e025a007a9f50e6e03e5aae9b9be8b527ca
sonlha <lhas1307@gmail.com>
2024-10-16 17:12:20 +0700

    fee selector

---
+++ apps/mobile/src/screens/components/fee-control/fee-selector.tsx
@@ -0,0 +1,142 @@
+import React, { FunctionComponent } from 'react';
+import { IFeeConfig } from '@owallet/hooks';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../../../stores';
+import { useStyle } from '../../../styles';
+import { Text, View } from 'react-native';
+import { FormattedMessage } from 'react-intl';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+
+export const FeeSelector: FunctionComponent<{
+  feeConfig: IFeeConfig;
+}> = observer(({ feeConfig }) => {
+  const { priceStore } = useStore();
+  const style = useStyle();
+
+  const feeCurrency = feeConfig.fees.length > 0 ? feeConfig.fees[0].currency : feeConfig.selectableFeeCurrencies[0];
+
+  if (!feeCurrency) {
+    return null;
+  }
+
+  return (
+    <View>
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            feeConfig.setFee({
+              type: 'low',
+              currency: feeCurrency
+            });
+          }}
+        >
+          <View
+            style={{
+              borderTopLeftRadius: 8,
+              borderBottomLeftRadius: 8,
+              borderRightWidth: 1,
+              borderRightColor: 'black'
+            }}
+          >
+            <Text>
+              <FormattedMessage id="components.input.fee-control.modal.fee-selector.low" />
+            </Text>
+
+            {feeCurrency.coinGeckoId ? (
+              <Text>
+                {priceStore.calculatePrice(feeConfig.getFeeTypePrettyForFeeCurrency(feeCurrency, 'low'))?.toString() ||
+                  '-'}
+              </Text>
+            ) : null}
+
+            <Text>
+              {feeConfig
+                .getFeeTypePrettyForFeeCurrency(feeCurrency, 'low')
+                .maxDecimals(6)
+                .inequalitySymbol(true)
+                .trim(true)
+                .hideIBCMetadata(true)
+                .toString()}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            feeConfig.setFee({
+              type: 'average',
+              currency: feeCurrency
+            });
+          }}
+        >
+          <View>
+            <Text>
+              <FormattedMessage id="components.input.fee-control.modal.fee-selector.average" />
+            </Text>
+
+            {feeCurrency.coinGeckoId ? (
+              <Text>
+                {priceStore
+                  .calculatePrice(feeConfig.getFeeTypePrettyForFeeCurrency(feeCurrency, 'average'))
+                  ?.toString() || '-'}
+              </Text>
+            ) : null}
+
+            <Text>
+              {feeConfig
+                .getFeeTypePrettyForFeeCurrency(feeCurrency, 'average')
+                .maxDecimals(6)
+                .inequalitySymbol(true)
+                .trim(true)
+                .hideIBCMetadata(true)
+                .toString()}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            feeConfig.setFee({
+              type: 'high',
+              currency: feeCurrency
+            });
+          }}
+        >
+          <View
+            style={{
+              borderTopRightRadius: 8,
+              borderBottomRightRadius: 8,
+              borderLeftWidth: 1,
+              borderLeftColor: 'black'
+            }}
+          >
+            <Text>
+              <FormattedMessage id="components.input.fee-control.modal.fee-selector.high" />
+            </Text>
+
+            {feeCurrency.coinGeckoId ? (
+              <Text>
+                {priceStore.calculatePrice(feeConfig.getFeeTypePrettyForFeeCurrency(feeCurrency, 'high'))?.toString() ||
+                  '-'}
+              </Text>
+            ) : null}
+
+            <Text>
+              {feeConfig
+                .getFeeTypePrettyForFeeCurrency(feeCurrency, 'high')
+                .maxDecimals(6)
+                .inequalitySymbol(true)
+                .trim(true)
+                .hideIBCMetadata(true)
+                .toString()}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+});
---
+++ apps/mobile/src/screens/components/fee-control/index.tsx
@@ -0,0 +1,264 @@
+import React, { FunctionComponent, useLayoutEffect, useState } from 'react';
+import { IFeeConfig, IGasConfig, IGasSimulator, InsufficientFeeError, ISenderConfig } from '@owallet/hooks';
+import { observer } from 'mobx-react-lite';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useStore } from '../../../stores';
+import { autorun } from 'mobx';
+import { CoinPretty, Dec, PricePretty } from '@owallet/unit';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { TransactionFeeModal } from './transaction-fee-modal';
+import { UIConfigStore } from '../../../stores/ui-config';
+import { IChainStore, IQueriesStore } from '@owallet/stores';
+import { OWButton } from '@src/components/button';
+import OWText from '@src/components/text/ow-text';
+import { useTheme } from '@src/themes/theme-provider';
+
+export const useFeeOptionSelectionOnInit = (
+  uiConfigStore: UIConfigStore,
+  feeConfig: IFeeConfig,
+  disableAutomaticFeeSet: boolean | undefined
+) => {
+  useLayoutEffect(() => {
+    if (disableAutomaticFeeSet) {
+      return;
+    }
+
+    if (feeConfig.fees.length === 0 && feeConfig.selectableFeeCurrencies.length > 0) {
+      if (uiConfigStore.rememberLastFeeOption && uiConfigStore.lastFeeOption) {
+        feeConfig.setFee({
+          type: uiConfigStore.lastFeeOption,
+          currency: feeConfig.selectableFeeCurrencies[0]
+        });
+      } else {
+        feeConfig.setFee({
+          type: 'average',
+          currency: feeConfig.selectableFeeCurrencies[0]
+        });
+      }
+    }
+  }, [
+    disableAutomaticFeeSet,
+    feeConfig,
+    feeConfig.fees,
+    feeConfig.selectableFeeCurrencies,
+    uiConfigStore.lastFeeOption,
+    uiConfigStore.rememberLastFeeOption
+  ]);
+};
+
+export const useAutoFeeCurrencySelectionOnInit = (
+  chainStore: IChainStore,
+  queriesStore: IQueriesStore,
+  senderConfig: ISenderConfig,
+  feeConfig: IFeeConfig,
+  disableAutomaticFeeSet: boolean | undefined
+) => {
+  useLayoutEffect(() => {
+    if (disableAutomaticFeeSet) {
+      return;
+    }
+
+    // Require to invoke effect whenever chain is changed,
+    // even though it is not used in logic.
+    noop(feeConfig.chainId);
+
+    // Try to find other fee currency if the account doesn't have enough fee to pay.
+    // This logic can be slightly complex, so use mobx's `autorun`.
+    // This part fairly different with the approach of react's hook.
+    let skip = false;
+    // Try until 500ms to avoid the confusion to user.
+    const timeoutId = setTimeout(() => {
+      skip = true;
+    }, 500);
+
+    const disposer = autorun(() => {
+      if (
+        !skip &&
+        feeConfig.type !== 'manual' &&
+        feeConfig.selectableFeeCurrencies.length > 1 &&
+        feeConfig.fees.length > 0
+      ) {
+        const queryBalances = queriesStore
+          .get(feeConfig.chainId)
+          .queryBalances.getQueryBech32Address(senderConfig.sender);
+
+        const currentFeeCurrency = feeConfig.fees[0].currency;
+        const currentFeeCurrencyBal = queryBalances.getBalanceFromCurrency(currentFeeCurrency);
+
+        const currentFee = feeConfig.getFeeTypePrettyForFeeCurrency(currentFeeCurrency, feeConfig.type);
+        if (currentFeeCurrencyBal.toDec().lt(currentFee.toDec())) {
+          const isOsmosis =
+            chainStore.hasChain(feeConfig.chainId) &&
+            chainStore.getChain(feeConfig.chainId).hasFeature('osmosis-txfees');
+
+          // Not enough balances for fee.
+          // Try to find other fee currency to send.
+          for (const feeCurrency of feeConfig.selectableFeeCurrencies) {
+            const feeCurrencyBal = queryBalances.getBalanceFromCurrency(feeCurrency);
+            const fee = feeConfig.getFeeTypePrettyForFeeCurrency(feeCurrency, feeConfig.type);
+
+            if (isOsmosis && fee.toDec().lte(new Dec(0))) {
+              continue;
+            }
+
+            if (feeCurrencyBal.toDec().gte(fee.toDec())) {
+              feeConfig.setFee({
+                type: feeConfig.type,
+                currency: feeCurrency
+              });
+              const uiProperties = feeConfig.uiProperties;
+              skip = !uiProperties.loadingState && uiProperties.error == null && uiProperties.warning == null;
+              return;
+            }
+          }
+        }
+      }
+    });
+
+    return () => {
+      clearTimeout(timeoutId);
+      skip = true;
+      disposer();
+    };
+  }, [chainStore, disableAutomaticFeeSet, feeConfig, feeConfig.chainId, queriesStore, senderConfig.sender]);
+};
+
+export const FeeControl: FunctionComponent<{
+  senderConfig: ISenderConfig;
+  feeConfig: IFeeConfig;
+  gasConfig: IGasConfig;
+  gasSimulator?: IGasSimulator;
+
+  disableAutomaticFeeSet?: boolean;
+}> = observer(({ senderConfig, feeConfig, gasConfig, gasSimulator, disableAutomaticFeeSet }) => {
+  const { queriesStore, priceStore, chainStore, uiConfigStore } = useStore();
+  const intl = useIntl();
+  const { colors } = useTheme();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+  const hasError = feeConfig.uiProperties.error || feeConfig.uiProperties.warning;
+
+  useFeeOptionSelectionOnInit(uiConfigStore, feeConfig, disableAutomaticFeeSet);
+
+  useAutoFeeCurrencySelectionOnInit(chainStore, queriesStore, senderConfig, feeConfig, disableAutomaticFeeSet);
+
+  return (
+    <View>
+      <View>
+        {disableAutomaticFeeSet ? (
+          <React.Fragment>
+            <OWButton onPress={() => setIsInfoModalOpen(true)} />
+          </React.Fragment>
+        ) : null}
+
+        <Text>
+          <FormattedMessage
+            id="components.input.fee-control.fee"
+            values={{
+              assets: (() => {
+                if (feeConfig.fees.length > 0) {
+                  return feeConfig.fees;
+                }
+                const chainInfo = chainStore.getChain(feeConfig.chainId);
+
+                return [new CoinPretty(chainInfo.stakeCurrency || chainInfo.currencies[0], new Dec(0))];
+              })()
+                .map(fee =>
+                  fee.maxDecimals(6).inequalitySymbol(true).trim(true).shrink(true).hideIBCMetadata(true).toString()
+                )
+                .join('+')
+            }}
+          />
+        </Text>
+
+        <Text>
+          {(() => {
+            let total: PricePretty | undefined;
+            let hasUnknown = false;
+            for (const fee of feeConfig.fees) {
+              if (!fee.currency.coinGeckoId) {
+                hasUnknown = true;
+                break;
+              } else {
+                const price = priceStore.calculatePrice(fee);
+                if (price) {
+                  if (!total) {
+                    total = price;
+                  } else {
+                    total = total.add(price);
+                  }
+                }
+              }
+            }
+
+            if (hasUnknown || !total) {
+              return '-';
+            }
+            return `(${total.toString()})`;
+          })()}
+        </Text>
+
+        {!disableAutomaticFeeSet && uiConfigStore.rememberLastFeeOption ? (
+          <React.Fragment>
+            <View />
+          </React.Fragment>
+        ) : null}
+
+        <OWButton onPress={() => setIsModalOpen(true)} />
+
+        {feeConfig.uiProperties.loadingState || gasSimulator?.uiProperties.loadingState ? (
+          <React.Fragment>
+            <View>
+              <ActivityIndicator color={'white'} size={20} />
+            </View>
+          </React.Fragment>
+        ) : null}
+      </View>
+
+      {hasError ? (
+        <View>
+          <OWText style={{ paddingBottom: 6 }} color={colors['warning-border-default']} weight="500" size={14}>
+            {(() => {
+              if (feeConfig.uiProperties.error) {
+                if (feeConfig.uiProperties.error instanceof InsufficientFeeError) {
+                  return intl.formatMessage({
+                    id: 'components.input.fee-control.error.insufficient-fee'
+                  });
+                }
+
+                return feeConfig.uiProperties.error.message || feeConfig.uiProperties.error.toString();
+              }
+
+              if (feeConfig.uiProperties.warning) {
+                return feeConfig.uiProperties.warning.message || feeConfig.uiProperties.warning.toString();
+              }
+
+              if (gasConfig.uiProperties.error) {
+                return gasConfig.uiProperties.error.message || gasConfig.uiProperties.error.toString();
+              }
+
+              if (gasConfig.uiProperties.warning) {
+                return gasConfig.uiProperties.warning.message || gasConfig.uiProperties.warning.toString();
+              }
+            })() ?? ''}
+          </OWText>
+        </View>
+      ) : null}
+
+      <TransactionFeeModal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+        senderConfig={senderConfig}
+        feeConfig={feeConfig}
+        gasConfig={gasConfig}
+        gasSimulator={gasSimulator}
+        disableAutomaticFeeSet={disableAutomaticFeeSet}
+      />
+    </View>
+  );
+});
+
+const noop = (..._args: any[]) => {
+  // noop
+};
