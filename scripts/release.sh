581ef354ccfe3072967ae88cf23264845f45f7ef
sonlha <lhas1307@gmail.com>
2024-10-17 18:38:23 +0700

    update extension path

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,11 @@
 set -ex
 validate_tag='v[0-9].[0-9]+.[0-9]+$'
 
-zip -r packages/extension/owallet-extension-${REF_NAME}.zip packages/extension/prod/ 
+zip -r apps/extension/owallet-extension-${REF_NAME}.zip apps/extension/prod/ 
 
 if [[ ${REF_NAME} =~ ${validate_tag} ]]; then
-    gh release create ${REF_NAME} packages/extension/owallet-extension-${REF_NAME}.zip --title ${REF_NAME}
+    gh release create ${REF_NAME} apps/extension/owallet-extension-${REF_NAME}.zip --title ${REF_NAME}
 else 
-    gh release create ${REF_NAME} packages/extension/owallet-extension-${REF_NAME}.zip --title ${REF_NAME} --prerelease
+    gh release create ${REF_NAME} apps/extension/owallet-extension-${REF_NAME}.zip --title ${REF_NAME} --prerelease
     echo "PRERELEASE=0" >> $GITHUB_ENV
 fi
