b7a6ecd08ca9758385c384580acf75868c691810
sonlha <lhas1307@gmail.com>
2024-11-21 11:22:04 +0700

    update InjectedProviderUrl

--- apps/mobile/src/screens/web/config.ts
+++ apps/mobile/src/screens/web/config.ts
@@ -1,9 +1,8 @@
 import { ImageSourcePropType } from 'react-native';
 
-export const InjectedProviderUrl =
-  process.env.INJECTED_PROVIDER_URL || 'https://static.orai.io/injected-provider-merge.bundle.js';
 // export const InjectedProviderUrl =
-//   'https://raw.githubusercontent.com/owallet-io/owallet-provider/refs/heads/test/build-bundle/injected-provider-keyring.bundle.js?token=GHSAT0AAAAAACM3LPFUXGJ55RR7YP4HUWJWZZNSRTQ';
+//   process.env.INJECTED_PROVIDER_URL || 'https://static.orai.io/injected-provider-merge.bundle.js';
+export const InjectedProviderUrl = 'https://static.orai.io/injected-provider-test.bundle.js';
 
 export type DAppInfo = {
   name: string;
