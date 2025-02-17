4f12d4dc9c50524aa50a1228e00c8fdbda1d1f3f
sonlha <lhas1307@gmail.com>
2025-02-17 10:31:01 +0700

    BridgeParsedItem

--- apps/extension/src/pages/sign/components/message-item/index.tsx
+++ apps/extension/src/pages/sign/components/message-item/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Column, Columns } from "../../../../components/column";
 import { Box } from "../../../../components/box";
 import { ColorPalette } from "../../../../styles";
@@ -12,6 +12,229 @@ import { camelCaseToTitleCase, mapToDynamicAction } from "./helper";
 import { shortenWord, toDisplay } from "@owallet/common";
 
 const ParsedItem: FunctionComponent<{
+  theme: any;
+  parsedMsg: any;
+}> = ({ theme, parsedMsg }) => {
+  return parsedMsg.action === "transfer_to_remote" ? (
+    <BridgeParsedItem parsedMsg={parsedMsg} theme={theme} />
+  ) : (
+    <SwapParsedItem parsedMsg={parsedMsg} theme={theme} />
+  );
+};
+const BridgeParsedItem: FunctionComponent<{
+  theme: any;
+  parsedMsg: any;
+}> = ({ theme, parsedMsg }) => {
+  const { data: prices } = useCoinGeckoPrices();
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setData(parsedMsg.response.data);
+  }, [parsedMsg]);
+
+  if (!data) return null;
+  console.log("data 2222", data);
+
+  const tokenPrice = prices?.[data?.tokenInfo?.coinGeckoId];
+  const feeAmount =
+    tokenPrice * toDisplay(data?.feeAmount, data?.tokenInfo.decimals);
+  const outValue =
+    tokenPrice * toDisplay(data?.bridgeAmount, data?.tokenInfo.decimals);
+
+  return (
+    <>
+      <Gutter size="2px" />
+      <Body3
+        color={
+          theme.mode === "light"
+            ? ColorPalette["gray-300"]
+            : ColorPalette["gray-200"]
+        }
+      >
+        <Gutter size="1rem" />
+        <XAxis
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 14,
+              fontWeight: "600",
+              color: ColorPalette["black-50"],
+            }}
+          >
+            Action
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <span
+              style={{
+                fontSize: 16,
+                fontWeight: "500",
+                color:
+                  theme.mode === "light"
+                    ? ColorPalette["platinum-200"]
+                    : ColorPalette["platinum-200"],
+              }}
+            >
+              {parsedMsg.action}
+            </span>
+          </div>
+        </XAxis>
+        <Gutter size="1.25rem" />
+        <div
+          style={{
+            width: "100%",
+            height: 0.75,
+            backgroundColor:
+              theme.mode === "light"
+                ? ColorPalette["gray-90"]
+                : ColorPalette["gray-90"],
+          }}
+        />
+        <Gutter size="1rem" />
+        <XAxis
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 14,
+              fontWeight: "600",
+              color: ColorPalette["black-50"],
+            }}
+          >
+            Token Info
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <span
+              style={{
+                fontSize: 16,
+                fontWeight: "500",
+                color:
+                  theme.mode === "light"
+                    ? ColorPalette["green-350"]
+                    : ColorPalette["green-350"],
+              }}
+            >
+              +{toDisplay(data.bridgeAmount, data.tokenInfo.decimals)}{" "}
+              {data.tokenInfo.denom.toUpperCase()}
+            </span>
+            <span
+              style={{
+                fontSize: 12,
+                color:
+                  theme.mode === "light"
+                    ? ColorPalette["gray-80"]
+                    : ColorPalette["gray-80"],
+                paddingTop: 4,
+              }}
+            >
+              ≈ ${!outValue ? "0" : outValue.toFixed(4).toString()}
+            </span>
+          </div>
+        </XAxis>
+        <Gutter size="0.25rem" />
+        <div
+          style={{
+            width: "100%",
+            height: 0.75,
+            backgroundColor:
+              theme.mode === "light"
+                ? ColorPalette["gray-90"]
+                : ColorPalette["gray-90"],
+          }}
+        />
+        <Gutter size="1rem" />
+        <XAxis
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 14,
+              fontWeight: "600",
+              color: ColorPalette["black-50"],
+            }}
+          >
+            Bridge Fee
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <span
+              style={{
+                fontSize: 16,
+                fontWeight: "500",
+                color:
+                  theme.mode === "light"
+                    ? ColorPalette["red-350"]
+                    : ColorPalette["red-350"],
+              }}
+            >
+              -{toDisplay(data.feeAmount, data.tokenInfo.decimals)}{" "}
+              {data.tokenInfo.denom.toUpperCase()}
+            </span>
+            <span
+              style={{
+                fontSize: 12,
+                color:
+                  theme.mode === "light"
+                    ? ColorPalette["gray-80"]
+                    : ColorPalette["gray-80"],
+                paddingTop: 4,
+              }}
+            >
+              ≈ ${!outValue ? "0" : outValue.toFixed(4).toString()}
+            </span>
+          </div>
+        </XAxis>
+        {/* {Object.keys(parsedMsg).map((key) => {
+          return (
+            <XAxis
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Subtitle3>{camelCaseToTitleCase(key)}</Subtitle3>
+              <Subtitle4>
+                {typeof parsedMsg[key] === "object"
+                  ? shortenWord(
+                      JSON.stringify(mapToDynamicAction(parsedMsg[key]))
+                    )
+                  : shortenWord(parsedMsg[key])}
+              </Subtitle4>
+            </XAxis>
+          );
+        })} */}
+      </Body3>
+    </>
+  );
+};
+
+const SwapParsedItem: FunctionComponent<{
   theme: any;
   parsedMsg: any;
 }> = ({ theme, parsedMsg }) => {
@@ -30,11 +253,13 @@ const ParsedItem: FunctionComponent<{
   };
 
   const { data: prices } = useCoinGeckoPrices();
-  const inPrice = prices?.[mockOutAsset.coinGeckoId];
+  const inPrice = prices?.[mockInAsset.coinGeckoId];
   const inValue = inPrice * toDisplay(parsedMsg.inAmount, mockInAsset.decimals);
   const outPrice = prices?.[mockOutAsset.coinGeckoId];
   const outValue =
     outPrice * toDisplay(parsedMsg.outAmount, mockOutAsset.decimals);
+
+  console.log("parsedMsg", parsedMsg);
 
   return (
     <>
@@ -168,25 +393,6 @@ const ParsedItem: FunctionComponent<{
                 : ColorPalette["gray-90"],
           }}
         />
-        {/* {Object.keys(parsedMsg).map((key) => {
-          return (
-            <XAxis
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Subtitle3>{camelCaseToTitleCase(key)}</Subtitle3>
-              <Subtitle4>
-                {typeof parsedMsg[key] === "object"
-                  ? shortenWord(
-                      JSON.stringify(mapToDynamicAction(parsedMsg[key]))
-                    )
-                  : shortenWord(parsedMsg[key])}
-              </Subtitle4>
-            </XAxis>
-          );
-        })} */}
       </Body3>
     </>
   );
@@ -199,7 +405,7 @@ export const MessageItem: FunctionComponent<{
   msg?: any;
   senderConfig?: SenderConfig;
 }> = ({ title, content, msg, senderConfig }) => {
-  const rpc = "http://192.168.10.119:9000/";
+  const rpc = "http://192.168.10.71:9000/";
   const theme = useTheme();
   const [parsedMsg, setParsedMsg] = React.useState<any>();
 
@@ -214,6 +420,7 @@ export const MessageItem: FunctionComponent<{
       },
       {}
     );
+    console.log("data parseMsg", data);
     if (data) {
       setParsedMsg(data.data);
     }
